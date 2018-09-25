f400c291342888c2c57d260ce63e570b43f4dc2c
Nick Van den Broeck <nick.van.den.broeck@hotmail.com>
2018-09-26 00:34:47 +0200

    smos-report: command 'next' is implemented

---
+++ smos-report/src/Smos/Report/ShouldPrint.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Smos.Report.ShouldPrint where
+
+import Import
+
+import Data.Configurator.Types
+
+import qualified Data.Text as T
+
+data ShouldPrint
+    = PrintError
+    | PrintWarning
+    | DontPrint
+    deriving (Show, Eq)
+
+parseShouldPrint :: String -> Maybe ShouldPrint
+parseShouldPrint "error" = Just PrintError
+parseShouldPrint "warning" = Just PrintWarning
+parseShouldPrint "nothing" = Just DontPrint
+parseShouldPrint _ = Nothing
+
+instance Configured ShouldPrint where
+    convert (String t) = parseShouldPrint $ T.unpack t
+    convert _ = Nothing
+
+printErrorMessages :: ShouldPrint -> String -> IO ()
+printErrorMessages DontPrint = const $ pure ()
+printErrorMessages PrintWarning = putStr
+printErrorMessages PrintError = error
