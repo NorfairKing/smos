c428dd6b0fd28720f4f3377fcd1d3c76fec0c44b
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-09-27 17:17:48 +0200

    Contents cursor

--- smos-cursor-gen/test/Smos/Cursor/ContentsSpec.hs
+++ smos-cursor-gen/test/Smos/Cursor/ContentsSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeApplications #-}
+
 module Smos.Cursor.ContentsSpec where
 
 import Test.Hspec
@@ -20,3 +21,65 @@ spec = do
             producesValidsOnValids rebuildContentsCursor
         it "is the inverse of makeContentsCursor" $
             inverseFunctionsOnValid makeContentsCursor rebuildContentsCursor
+    describe "makeContentsCursorWithSelection" $ do
+        it "produces valid cursors" $
+            producesValidsOnValids3 makeContentsCursorWithSelection
+        it "rebuilds the current cursor when given the current selection" $
+            forAllValid $ \cc ->
+                let (x, y) = contentsCursorSelection cc
+                in makeContentsCursorWithSelection
+                       x
+                       y
+                       (rebuildContentsCursor cc) `shouldBe`
+                   Just cc
+    describe "contentsCursorSelection" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorSelection
+    describe "contentsCursorSelectPrevLine" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorSelectPrevLine
+    describe "contentsCursorSelectNextLine" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorSelectNextLine
+    describe "contentsCursorSelectFirstLine" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorSelectFirstLine
+    describe "contentsCursorSelectLastLine" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorSelectLastLine
+    describe "contentsCursorSelectPrevChar" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorSelectPrevChar
+    describe "contentsCursorSelectNextChar" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorSelectNextChar
+    describe "contentsCursorIndexOnLine" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorIndexOnLine
+    describe "contentsCursorSelectIndexOnLine" $
+        it "produces valid cursors" $
+        producesValidsOnValids2 contentsCursorSelectIndexOnLine
+    describe "contentsCursorInsertChar" $
+        it "produces valid cursors" $
+        producesValidsOnValids2 contentsCursorInsertChar
+    describe "contentsCursorAppendChar" $
+        it "produces valid cursors" $
+        producesValidsOnValids2 contentsCursorAppendChar
+    describe "contentsCursorInsertNewline" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorInsertNewline
+    describe "contentsCursorAppendNewline" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorAppendNewline
+    describe "contentsCursorRemove" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorRemove
+    describe "contentsCursorDelete" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorDelete
+    describe "contentsCursorSelectStartOfLine" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorSelectStartOfLine
+    describe "contentsCursorSelectEndOfLine" $
+        it "produces valid cursors" $
+        producesValidsOnValids contentsCursorSelectEndOfLine
--- smos-cursor/src/Smos/Cursor/Contents.hs
+++ smos-cursor/src/Smos/Cursor/Contents.hs
@@ -1,4 +1,32 @@
-module Smos.Cursor.Contents where
+module Smos.Cursor.Contents
+    ( ContentsCursor(..)
+    , emptyContentsCursor
+    , makeContentsCursor
+    , makeContentsCursorWithSelection
+    , rebuildContentsCursor
+    , contentsCursorSelection
+    , contentsCursorSelectPrevLine
+    , contentsCursorSelectNextLine
+    , contentsCursorSelectFirstLine
+    , contentsCursorSelectLastLine
+    , contentsCursorSelectPrevChar
+    , contentsCursorSelectNextChar
+    , contentsCursorIndexOnLine
+    , contentsCursorSelectIndexOnLine
+    , contentsCursorInsertChar
+    , contentsCursorAppendChar
+    , contentsCursorInsertNewline
+    , contentsCursorAppendNewline
+    , contentsCursorRemove
+    , contentsCursorDelete
+    , contentsCursorSelectStartOfLine
+    , contentsCursorSelectEndOfLine
+    ) where
+
+import Data.List.NonEmpty (NonEmpty)
+import Data.Text (Text)
+
+import Lens.Micro
 
 import Cursor.TextField
 
@@ -6,8 +34,66 @@ import Smos.Data.Types
 
 type ContentsCursor = TextFieldCursor
 
+emptyContentsCursor :: ContentsCursor
+emptyContentsCursor = emptyTextFieldCursor
+
 makeContentsCursor :: Contents -> ContentsCursor
 makeContentsCursor = makeTextFieldCursor . contentsText
 
+makeContentsCursorWithSelection ::
+       Int -> Int -> Contents -> Maybe ContentsCursor
+makeContentsCursorWithSelection x y = makeTextFieldCursorWithSelection x y . contentsText
+
 rebuildContentsCursor :: ContentsCursor -> Contents
 rebuildContentsCursor = Contents . rebuildTextFieldCursor
+
+contentsCursorSelection :: ContentsCursor -> (Int, Int)
+contentsCursorSelection = textFieldCursorSelection
+
+contentsCursorSelectPrevLine :: ContentsCursor -> Maybe ContentsCursor
+contentsCursorSelectPrevLine = textFieldCursorSelectPrevLine
+
+contentsCursorSelectNextLine :: ContentsCursor -> Maybe ContentsCursor
+contentsCursorSelectNextLine = textFieldCursorSelectNextLine
+
+contentsCursorSelectFirstLine :: ContentsCursor -> ContentsCursor
+contentsCursorSelectFirstLine = textFieldCursorSelectFirstLine
+
+contentsCursorSelectLastLine :: ContentsCursor -> ContentsCursor
+contentsCursorSelectLastLine = textFieldCursorSelectLastLine
+
+contentsCursorSelectPrevChar :: ContentsCursor -> Maybe ContentsCursor
+contentsCursorSelectPrevChar = textFieldCursorSelectPrevChar
+
+contentsCursorSelectNextChar :: ContentsCursor -> Maybe ContentsCursor
+contentsCursorSelectNextChar = textFieldCursorSelectNextChar
+
+contentsCursorIndexOnLine :: ContentsCursor -> Int
+contentsCursorIndexOnLine = textFieldCursorIndexOnLine
+
+contentsCursorSelectIndexOnLine :: Int -> ContentsCursor -> ContentsCursor
+contentsCursorSelectIndexOnLine = textFieldCursorSelectIndexOnLine
+
+contentsCursorInsertChar :: Char -> ContentsCursor -> ContentsCursor
+contentsCursorInsertChar = textFieldCursorInsertChar
+
+contentsCursorAppendChar :: Char -> ContentsCursor -> ContentsCursor
+contentsCursorAppendChar = textFieldCursorAppendChar
+
+contentsCursorInsertNewline :: ContentsCursor -> ContentsCursor
+contentsCursorInsertNewline = textFieldCursorInsertNewline
+
+contentsCursorAppendNewline :: ContentsCursor -> ContentsCursor
+contentsCursorAppendNewline = textFieldCursorAppendNewline
+
+contentsCursorRemove :: ContentsCursor -> Maybe ContentsCursor
+contentsCursorRemove = textFieldCursorRemove
+
+contentsCursorDelete :: ContentsCursor -> Maybe ContentsCursor
+contentsCursorDelete = textFieldCursorDelete
+
+contentsCursorSelectStartOfLine :: ContentsCursor -> ContentsCursor
+contentsCursorSelectStartOfLine = textFieldCursorSelectStartOfLine
+
+contentsCursorSelectEndOfLine :: ContentsCursor -> ContentsCursor
+contentsCursorSelectEndOfLine = textFieldCursorSelectEndOfLine
