642ee0a38bd9c1293945577697b015ddf679515d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-09-28 20:44:24 +0200

    dependencies on fuzzy-time and cursor-fuzzy-time

---
+++ nix/cursor-fuzzy-time-version.nix
@@ -0,0 +1,6 @@
+{
+  owner = "NorfairKing";
+  repo = "cursor-fuzzy-time";
+  rev = "cf74eb27fb785a3c0bc69234f6d248937fa65df1";
+  sha256 = "1ni4ayl2gzpyk8hk017nnpyd18yzadbnmmjln5xglb6zd3dasi5l";
+}
