2b146ac09804799e866327d01cdbf1678bff84fa
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-10-02 07:37:17 +0200

    little refactor

--- smos-report/src/Smos/Report/Clock.hs
+++ smos-report/src/Smos/Report/Clock.hs
@@ -5,7 +5,6 @@ module Smos.Report.Clock where
 
 import GHC.Generics (Generic)
 
-import Data.List
 import Data.Maybe
 
 import qualified Data.Text as T
@@ -14,19 +13,15 @@ import qualified Data.Text.IO as T
 import Data.Time
 import Data.Tree
 
-import Text.Show.Pretty
-
 import Path
-import Path.IO
 
 import Conduit
-import qualified Data.Conduit.Combinators as C
 
 import Smos.Data
 
 import Smos.Report.Formatting
 import Smos.Report.OptParse
-import Smos.Report.Parse
+import Smos.Report.Streaming
 
 clock :: Settings -> IO ()
 clock Settings {..} = do
@@ -35,52 +30,6 @@ clock Settings {..} = do
         sourceNonhiddenFiles setWorkDir .| filterSmosFiles .|
         parseSmosFiles setWorkDir setShouldPrint
     T.putStrLn $ renderClockTable $ makeClockTable tups
-
-sourceNonhiddenFiles :: Path Abs Dir -> ConduitT i (Path Rel File) IO ()
-sourceNonhiddenFiles dir = walkDir go dir
-  where
-    go :: Path Abs Dir
-       -> [Path Abs Dir]
-       -> [Path Abs File]
-       -> ConduitT i (Path Rel File) IO WalkAction
-    go curdir subdirs files = do
-        C.yieldMany $ mapMaybe (stripProperPrefix dir) files
-        pure $ WalkExclude $ filter hidden subdirs
-      where
-        hidden ad =
-            case stripProperPrefix curdir ad of
-                Nothing -> True
-                Just rd -> ("." `isPrefixOf` fromRelDir rd)
-
-filterSmosFiles :: Monad m => ConduitT (Path r File) (Path r File) m ()
-filterSmosFiles = C.filter $ (== ".smos") . fileExtension
-
-parseSmosFiles ::
-       Path Abs Dir
-    -> ShouldPrint
-    -> ConduitT (Path Rel File) (Path Rel File, SmosFile) IO ()
-parseSmosFiles dir sp = loop
-  where
-    loop = do
-        mp <- await
-        case mp of
-            Nothing -> pure ()
-            Just p -> do
-                let ap = dir </> p
-                mErrOrSmosFile <- lift $ readSmosFile ap
-                case mErrOrSmosFile of
-                    Nothing ->
-                        lift $
-                        printErrorMessages sp $
-                        displayErrMess [FileDoesntExist ap]
-                    Just errOrSmosFile ->
-                        case errOrSmosFile of
-                            Left err ->
-                                lift $
-                                printErrorMessages sp $
-                                displayErrMess [SmosFileParseError ap err]
-                            Right sf -> yield (p, sf)
-                loop
 
 data ClockTableEntry = ClockTableEntry
     { clockTableEntryFile :: Path Rel File
---
+++ smos-report/src/Smos/Report/Streaming.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Smos.Report.Streaming where
+
+import Data.List
+import Data.Maybe
+
+import Path
+import Path.IO
+
+import Conduit
+import qualified Data.Conduit.Combinators as C
+
+import Smos.Data
+
+import Smos.Report.OptParse
+import Smos.Report.Parse
+
+sourceNonhiddenFiles :: Path Abs Dir -> ConduitT i (Path Rel File) IO ()
+sourceNonhiddenFiles dir = walkDir go dir
+  where
+    go :: Path Abs Dir
+       -> [Path Abs Dir]
+       -> [Path Abs File]
+       -> ConduitT i (Path Rel File) IO WalkAction
+    go curdir subdirs files = do
+        C.yieldMany $ mapMaybe (stripProperPrefix dir) files
+        pure $ WalkExclude $ filter hidden subdirs
+      where
+        hidden ad =
+            case stripProperPrefix curdir ad of
+                Nothing -> True
+                Just rd -> ("." `isPrefixOf` fromRelDir rd)
+
+filterSmosFiles :: Monad m => ConduitT (Path r File) (Path r File) m ()
+filterSmosFiles = C.filter $ (== ".smos") . fileExtension
+
+parseSmosFiles ::
+       Path Abs Dir
+    -> ShouldPrint
+    -> ConduitT (Path Rel File) (Path Rel File, SmosFile) IO ()
+parseSmosFiles dir sp = loop
+  where
+    loop = do
+        mp <- await
+        case mp of
+            Nothing -> pure ()
+            Just p -> do
+                let ap = dir </> p
+                mErrOrSmosFile <- lift $ readSmosFile ap
+                case mErrOrSmosFile of
+                    Nothing ->
+                        lift $
+                        printErrorMessages sp $
+                        displayErrMess [FileDoesntExist ap]
+                    Just errOrSmosFile ->
+                        case errOrSmosFile of
+                            Left err ->
+                                lift $
+                                printErrorMessages sp $
+                                displayErrMess [SmosFileParseError ap err]
+                            Right sf -> yield (p, sf)
+                loop
