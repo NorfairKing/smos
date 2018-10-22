cb39c359b076895649991ff644aa67f3208ef8ad
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-10-22 07:55:32 +0200

    Another convenience function

--- smos/src/Smos/Actions/Convenience.hs
+++ smos/src/Smos/Actions/Convenience.hs
@@ -3,6 +3,10 @@
 module Smos.Actions.Convenience where
 
 import Control.Monad
+import Data.Time
+import Lens.Micro.Extras
+
+import Smos.Data
 
 import Smos.Types
 
@@ -18,13 +22,54 @@ convDoneAndWaitForResponse =
         , actionFunc =
               do modifyMTodoStateM $ const $ Just "DONE"
                  modifyFileCursor smosFileCursorInsertEntryAfterAndSelectHeader
-                 modifyHeaderCursorWhenSelectedM $ \hc ->
-                     foldM
-                         (flip headerCursorInsert)
-                         hc
-                         ("for a response" :: [Char])
+                 insertHeaderString "for a response"
                  modifyMTodoStateM $ const $ Just "WAITING"
                  modifyEntryCursor entryCursorSelectWhole
         , actionDescription =
               "Mark the current task as 'done' and add a new WAITING entry below"
         }
+
+convRepinged :: Action
+convRepinged =
+    Action
+        { actionName = "convRepinged"
+        , actionFunc =
+              do me <-
+                     gets
+                         (fmap
+                              (rebuildEntryCursor .
+                               view (smosFileCursorSelectedEntryL)) .
+                          view editorCursorSmosFileCursorL . smosStateCursor)
+                 case me of
+                     Nothing -> pure ()
+                     Just e -> do
+                         now <- liftIO getCurrentTime
+                         let e' =
+                                 e
+                                     { entryStateHistory =
+                                           StateHistory
+                                               [ StateHistoryEntry
+                                                     { stateHistoryEntryNewState =
+                                                           entryState e
+                                                     , stateHistoryEntryTimestamp =
+                                                           now
+                                                     }
+                                               ]
+                                     }
+                         modifyMTodoStateM $ const $ Just "DONE"
+                         modifyFileCursor
+                             smosFileCursorInsertEntryAfterAndSelectHeader
+                         insertHeaderString "Ping again"
+                         modifyMTodoStateM $ const $ Just "DONE"
+                         modifyFileCursor
+                             smosFileCursorInsertEntryAfterAndSelectHeader
+                         modifyEntryCursor $ const $ makeEntryCursor e'
+                         modifyEntryCursor entryCursorSelectWhole
+        , actionDescription =
+              "Mark the current task as 'done', add a new entry called 'Ping again' and add a new WAITING entry below that, that duplicates the original entry."
+        }
+
+insertHeaderString :: String -> SmosM ()
+insertHeaderString s =
+    modifyHeaderCursorWhenSelectedM $ \hc ->
+        foldM (flip headerCursorInsert) hc s
