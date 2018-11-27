a9033e16e7462e754767d6a71554f6866963c977
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-11-27 12:54:00 +0100

    improved the smos-query projects output

--- smos-report/src/Smos/Report/Projects.hs
+++ smos-report/src/Smos/Report/Projects.hs
@@ -7,6 +7,8 @@ module Smos.Report.Projects where
 
 import GHC.Generics (Generic)
 
+import Control.Applicative
+import Control.Monad
 import Data.Validity
 
 import Smos.Data
@@ -23,16 +25,31 @@ instance Validity ProjectEntry
 makeProjectEntry :: RootedPath -> SmosFile -> ProjectEntry
 makeProjectEntry rp sf =
     ProjectEntry
-    {projectEntryFilePath = rp, projectEntryCurrentEntry = getCurrentEntry sf}
+        { projectEntryFilePath = rp
+        , projectEntryCurrentEntry = getCurrentEntry sf
+        }
 
 getCurrentEntry :: SmosFile -> Maybe Entry
 getCurrentEntry = goF . smosFileForest
   where
-    goF f =
-        case reverse f of
-            [] -> Nothing
-            (lt:_) -> goT lt
+    goF :: Forest Entry -> Maybe Entry
+    goF f = msum $ map goT $ reverse f
+    goT :: Tree Entry -> Maybe Entry
     goT (Node e f) =
-        case reverse f of
-            [] -> Just e
-            _ -> goF f
+        case entryState e of
+            Nothing -> Nothing
+            Just ts ->
+                if isDone ts
+                    then Nothing
+                    else (case reverse f of
+                              _ -> goF f) <|>
+                         if isCurrent ts
+                             then Just e
+                             else Nothing
+    isDone :: TodoState -> Bool
+    isDone "DONE" = True
+    isDone "CANCELLED" = True
+    isDone _ = False
+    isCurrent :: TodoState -> Bool
+    isCurrent "TODO" = False
+    isCurrent _ = True
