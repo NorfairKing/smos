149af1bf970521016372bfb9f879b59ad3733be5
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-07-19 22:05:53 +0200

    made smos-report aware of the archive

--- smos-report/src/Smos/Report/Config.hs
+++ smos-report/src/Smos/Report/Config.hs
@@ -4,7 +4,8 @@
 
 module Smos.Report.Config
   ( SmosReportConfig(..)
-  , defaultReportConfig, defaultWorkflowDirSpec
+  , defaultReportConfig
+  , defaultWorkflowDirSpec
   , WorkflowDirSpec(..)
   , resolveWorkflowDir
   ) where
@@ -17,22 +18,37 @@ import Path.IO
 data SmosReportConfig =
   SmosReportConfig
     { smosReportConfigAgendaFileSpec :: !WorkflowDirSpec
+    , smosReportConfigArchiveFileSpec :: !ArchiveDirSpec
     }
   deriving (Show, Eq, Generic)
 
 defaultReportConfig :: SmosReportConfig
-defaultReportConfig = SmosReportConfig {smosReportConfigAgendaFileSpec = defaultWorkflowDirSpec}
-
-defaultWorkflowDirSpec :: WorkflowDirSpec
-defaultWorkflowDirSpec = DirInHome [reldir|workflow|]
+defaultReportConfig = SmosReportConfig {smosReportConfigAgendaFileSpec = defaultWorkflowDirSpec, smosReportConfigArchiveFileSpec = defaultArchiveDirSpec}
 
 data WorkflowDirSpec
   = DirInHome (Path Rel Dir)
   | DirAbsolute (Path Abs Dir)
   deriving (Show, Eq, Generic)
 
+defaultWorkflowDirSpec :: WorkflowDirSpec
+defaultWorkflowDirSpec = DirInHome [reldir|workflow|]
+
 resolveWorkflowDir :: WorkflowDirSpec -> IO (Path Abs Dir)
 resolveWorkflowDir afs =
   case afs of
     DirInHome rp -> getHomeDir >>= (`resolveDir` fromRelDir rp)
     DirAbsolute ad -> pure ad
+
+data ArchiveDirSpec
+  = ArchiveInWorkflow (Path Rel Dir)
+  | ArchiveAbsolute (Path Abs Dir)
+  deriving (Show, Eq, Generic)
+
+defaultArchiveDirSpec :: ArchiveDirSpec
+defaultArchiveDirSpec = ArchiveInWorkflow [reldir|archive|]
+
+resolveArchiveDir :: Path Abs Dir -> ArchiveDirSpec -> Path Abs Dir
+resolveArchiveDir wd as =
+  case as of
+    ArchiveInWorkflow ard -> wd </> ard
+    ArchiveAbsolute aad -> aad
