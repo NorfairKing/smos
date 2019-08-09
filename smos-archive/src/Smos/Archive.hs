26714869d8d15b139088d4a951803e6d5873dd65
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-08-09 16:27:56 +0200

    Archive learnt to warn if not everything is done in the file

--- smos-archive/src/Smos/Archive.hs
+++ smos-archive/src/Smos/Archive.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
@@ -10,6 +11,11 @@ import Data.Time
 import Path
 import Path.IO
 import System.Exit
+import System.IO (hFlush, stdout)
+
+import Data.Tree
+
+import Smos.Data
 
 import Smos.Archive.Config
 import Smos.Archive.OptParse
@@ -21,8 +27,10 @@ smosArchive = runReaderT $ liftIO getSettings >>= archive
 archive :: Settings -> Q ()
 archive Settings {..} = do
   let from = setFile
-  to <- getToFile setFile
-  moveToArchive from to
+  to <- getToFile from
+  liftIO $ do
+    checkFromFile from
+    moveToArchive from to
 
 getToFile :: Path Abs File -> Q (Path Abs File)
 getToFile file = do
@@ -47,15 +55,64 @@ getToFile file = do
       arf'' <- setFileExtension ext arf'
       pure $ archiveDir </> arf''
 
-moveToArchive :: Path Abs File -> Path Abs File -> Q ()
-moveToArchive from to =
-  liftIO $ do
-    ensureDir $ parent to
-    e1 <- doesFileExist from
-    if not e1
-      then die $ unwords ["The file to archive does not exist:", fromAbsFile from]
-      else do
-        e2 <- doesFileExist to
-        if e2
-          then die $ unwords ["Proposed archive file", fromAbsFile to, "already exists."]
-          else renameFile from to
+checkFromFile :: Path Abs File -> IO ()
+checkFromFile from = do
+  mErrOrSF <- readSmosFile from
+  case mErrOrSF of
+    Nothing -> die $ unwords ["File does not exist:", fromAbsFile from]
+    Just (Left e) ->
+      die $ unlines [unwords ["Failed to read file to archive:", fromAbsFile from], e]
+    Just (Right sf) ->
+      unless (all (isDone . entryState) (concatMap flatten (smosFileForest sf))) $ do
+        res <-
+          promptYesNo No $
+          unwords
+            [ "Not all entries in"
+            , fromAbsFile from
+            , "are done. Are you sure that you want to archive it?"
+            ]
+        case res of
+          Yes -> pure ()
+          No -> die "Not archiving."
+
+isDone :: Maybe TodoState -> Bool
+isDone (Just "DONE") = True
+isDone (Just "CANCELLED") = True
+isDone _ = True
+
+promptYesNo :: YesNo -> String -> IO YesNo
+promptYesNo def p = do
+  let defaultString =
+        case def of
+          Yes -> "[Y/n]"
+          No -> "[y/N]"
+  rs <- promptRawString $ p ++ " " ++ defaultString
+  case rs of
+    "yes" -> pure Yes
+    "y" -> pure Yes
+    "no" -> pure No
+    "n" -> pure No
+    _ -> pure def
+
+data YesNo
+  = Yes
+  | No
+  deriving (Show, Eq)
+
+promptRawString :: String -> IO String
+promptRawString s = do
+  putStr $ s ++ " > "
+  hFlush stdout
+  getLine
+
+moveToArchive :: Path Abs File -> Path Abs File -> IO ()
+moveToArchive from to = do
+  ensureDir $ parent to
+  e1 <- doesFileExist from
+  if not e1
+    then die $ unwords ["The file to archive does not exist:", fromAbsFile from]
+    else do
+      e2 <- doesFileExist to
+      if e2
+        then die $ unwords ["Proposed archive file", fromAbsFile to, "already exists."]
+        else renameFile from to
