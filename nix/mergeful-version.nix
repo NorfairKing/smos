58be81f2947f804c06bbd03ec4e6edf9990c31dd
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-09-11 10:22:50 +0200

    upgraded mergeful version

--- nix/mergeful-version.nix
+++ nix/mergeful-version.nix
@@ -1,6 +1,6 @@
 {
   owner = "NorfairKing";
   repo = "mergeful";
-  rev = "b14dde7a01cb12d7a2eec180c03de135059e36c6";
-  sha256 = "1nbh3w6wfpcgkgqqfbqr06kvzwzpwf55vmslj4cri7n2917gb9y4";
+  rev = "5370b8c2a6e8f532450ef6f814787002530095e1";
+  sha256 = "1p5hwg1xqzyllr4fvbj6d7hh3k720qjy90wj2wg6zqi88qmd13r5";
 }
