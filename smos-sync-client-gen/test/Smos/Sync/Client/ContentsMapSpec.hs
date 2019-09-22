3f1f2e6f743bd5f1409079fd5fac72e2a284b558
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-09-22 21:31:49 +0200

    something that tests, but doesn't pass tests yet

--- smos-sync-client-gen/test/Smos/Sync/Client/ContentsMapSpec.hs
+++ smos-sync-client-gen/test/Smos/Sync/Client/ContentsMapSpec.hs
@@ -5,66 +5,69 @@ module Smos.Sync.Client.ContentsMapSpec
   ) where
 
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 import Test.Validity
 import Test.Validity.Optics
 
 import qualified Smos.Sync.Client.ContentsMap as CM
-import Smos.Sync.Client.ContentsMap (ContentsMap(..), contentsMapL)
+import Smos.Sync.Client.ContentsMap (ContentsMap(..))
 import Smos.Sync.Client.ContentsMap.Gen
 
 spec :: Spec
-spec = do
-  genValidSpec @ContentsMap
-  describe "mapWithNewPath" $
-    it "generates valid values" $
-    forAllValid $ \cm -> forAllValid $ \bs -> genGeneratesValid (mapWithNewPath cm bs) (const [])
-  describe "mapsWithDifferentContentsAtNewPath" $
-    it "generates valid values" $
-    forAllValid $ \cm -> genGeneratesValid (mapsWithDifferentContentsAtNewPath cm) (const [])
-  describe "mapsWithDifferentContentsAtNewPath3" $
-    it "generates valid values" $
-    forAllValid $ \cm -> genGeneratesValid (mapsWithDifferentContentsAtNewPath3 cm) (const [])
-  describe "changedContentsMap" $
-    it "generates valid values" $
-    forAllValid $ \cm -> genGeneratesValid (changedContentsMap cm) (const [])
-  describe "changedMapsWithUnionOf" $
-    it "generates valid values" $
-    forAllValid $ \cm -> genGeneratesValid (changedMapsWithUnionOf cm) (const [])
-  describe "mapWithAdditions" $
-    it "generates valid values" $
-    forAllValid $ \cm -> genGeneratesValid (mapWithAdditions cm) (const [])
-  describe "twoDistinctPathsThatFitAndTheirUnion" $
-    it "generates valid values" $
-    forAllValid $ \bs1 ->
-      forAllValid $ \bs2 ->
-        genGeneratesValid (twoDistinctPathsThatFitAndTheirUnion bs1 bs2) (const [])
-  describe "twoDistinctPathsThatFitAndTheirUnionWith" $
-    it "generates valid values" $
-    forAllValid $ \m ->
+spec =
+  modifyMaxSize (* 10) $
+  modifyMaxSuccess (* 10) $ do
+    genValidSpec @ContentsMap
+    describe "mapWithNewPath" $
+      it "generates valid values" $
+      forAllValid $ \cm -> forAllValid $ \bs -> forAll (mapWithNewPath cm bs) shouldBeValid
+    describe "mapsWithDifferentContentsAtNewPath" $
+      it "generates valid values" $
+      forAllValid $ \cm -> forAll (mapsWithDifferentContentsAtNewPath cm) shouldBeValid
+    describe "mapsWithDifferentContentsAtNewPath3" $
+      it "generates valid values" $
+      forAllValid $ \cm -> forAll (mapsWithDifferentContentsAtNewPath3 cm) shouldBeValid
+    describe "changedContentsMap" $
+      it "generates valid values" $
+      forAllValid $ \cm -> forAll (changedContentsMap cm) shouldBeValid
+    describe "changedMapsWithUnionOf" $
+      it "generates valid values" $
+      forAllValid $ \cm -> forAll (changedMapsWithUnionOf cm) shouldBeValid
+    describe "mapWithAdditions" $
+      it "generates valid values" $ forAllValid $ \cm -> forAll (mapWithAdditions cm) shouldBeValid
+    describe "twoDistinctPathsThatFitAndTheirUnion" $
+      it "generates valid values" $
       forAllValid $ \bs1 ->
-        forAllValid $ \bs2 ->
-          genGeneratesValid (twoDistinctPathsThatFitAndTheirUnionWith m bs1 bs2) (const [])
-  describe "twoDistinctPathsThatFitAndTheirUnionsWith" $
-    it "generates valid values" $
-    forAllValid $ \m ->
-      forAllValid $ \bs1 ->
-        forAllValid $ \bs2 ->
-          genGeneratesValid (twoDistinctPathsThatFitAndTheirUnionsWith m bs1 bs2) (const [])
-  describe "disjunctContentsMap" $
-    it "generates valid values" $
-    forAllValid $ \cm -> genGeneratesValid (disjunctContentsMap cm) (const [])
-  describe "mapWithDisjunctUnion" $
-    it "generates valid values" $
-    forAllValid $ \cm -> genGeneratesValid (mapWithDisjunctUnion cm) (const [])
-  describe "twoChangedMapsAndTheirUnions" $
-    it "generates valid values" $ genGeneratesValid twoChangedMapsAndTheirUnions (const [])
-  describe "twoChangedMapsAndTheirUnionsWith" $
-    it "generates valid values" $
-    forAllValid $ \cm -> genGeneratesValid (twoChangedMapsAndTheirUnionsWith cm) (const [])
-  describe "threeDisjunctMapsAndTheirUnions" $
-    it "generates valid values" $ genGeneratesValid threeDisjunctMapsAndTheirUnions (const [])
-  describe "contentsMapL" $ lensSpecOnValid contentsMapL
-  describe "empty" $ it "is valid" $ shouldBeValid CM.empty
-  describe "singleton" $ it "produces valid contents maps" $ producesValidsOnValids2 CM.singleton
-  describe "insert" $ it "produces valid contents maps" $ producesValidsOnValids3 CM.insert
-  describe "union" $ it "produces valid contents maps" $ producesValidsOnValids2 CM.union
+        forAllValid $ \bs2 -> forAll (twoDistinctPathsThatFitAndTheirUnion bs1 bs2) shouldBeValid
+    describe "twoDistinctPathsThatFitAndTheirUnionWith" $
+      it "generates valid values" $
+      forAllValid $ \m ->
+        forAllValid $ \bs1 ->
+          forAllValid $ \bs2 ->
+            forAll (twoDistinctPathsThatFitAndTheirUnionWith m bs1 bs2) shouldBeValid
+    describe "twoDistinctPathsThatFitAndTheirUnionsWith" $
+      it "generates valid values" $
+      forAllValid $ \m ->
+        forAllValid $ \bs1 ->
+          forAllValid $ \bs2 ->
+            forAll (twoDistinctPathsThatFitAndTheirUnionsWith m bs1 bs2) shouldBeValid
+    describe "disjunctContentsMap" $
+      it "generates valid values" $
+      forAllValid $ \cm -> forAll (disjunctContentsMap cm) shouldBeValid
+    describe "mapWithDisjunctUnion" $
+      it "generates valid values" $
+      forAllValid $ \cm -> forAll (mapWithDisjunctUnion cm) shouldBeValid
+    describe "twoChangedMapsAndTheirUnions" $
+      it "generates valid values" $ forAll twoChangedMapsAndTheirUnions shouldBeValid
+    describe "twoChangedMapsAndTheirUnionsWith" $
+      it "generates valid values" $
+      forAllValid $ \cm -> forAll (twoChangedMapsAndTheirUnionsWith cm) shouldBeValid
+    describe "threeDisjunctMapsAndTheirUnions" $
+      it "generates valid values" $ forAll threeDisjunctMapsAndTheirUnions shouldBeValid
+    describe "empty" $ it "is valid" $ shouldBeValid CM.empty
+    describe "singleton" $ it "produces valid contents maps" $ producesValidsOnValids2 CM.singleton
+    describe "insert" $ it "produces valid contents maps" $ producesValidsOnValids3 CM.insert
+    describe "union" $ it "produces valid contents maps" $ producesValidsOnValids2 CM.union
+    describe "makeDirForest" $
+      it "produces valid DirForests" $ producesValidsOnValids CM.makeDirForest
--- smos-sync-client/src/Smos/Sync/Client/ContentsMap.hs
+++ smos-sync-client/src/Smos/Sync/Client/ContentsMap.hs
@@ -1,15 +1,28 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
 
 -- |
 --
 -- Import this module qualified
-module Smos.Sync.Client.ContentsMap where
+module Smos.Sync.Client.ContentsMap
+  ( ContentsMap(..)
+  , contentsMapFiles
+  , empty
+  , singleton
+  , insert
+  , union
+  , DirForest(..)
+  , makeDirForest
+  ) where
+
+import Debug.Trace
 
 import GHC.Generics (Generic)
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as SB
+import Data.Function
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe
@@ -29,6 +42,7 @@ import Control.Monad
 
 import Path
 import Path.IO
+import Path.Internal
 
 import Smos.Report.Streaming
 
@@ -42,10 +56,15 @@ newtype ContentsMap =
     }
   deriving (Show, Eq, Generic)
 
-instance Validity ContentsMap
-
-contentsMapL :: Lens' ContentsMap (Map (Path Rel File) ByteString)
-contentsMapL = lens contentsMapFiles $ \cm m -> cm {contentsMapFiles = m}
+instance Validity ContentsMap where
+  validate cm =
+    mconcat
+      [ genericValidate cm
+      , decorate "The map can be translated to a valid DirForest" $
+        case makeDirForest $ contentsMapFiles cm of
+          Left fp -> invalid $ "Duplicate path: " <> fp
+          Right df -> validate df
+      ]
 
 empty :: ContentsMap
 empty = ContentsMap M.empty
@@ -58,3 +77,51 @@ insert k v (ContentsMap m) = constructValid $ ContentsMap $ M.insert k v m
 
 union :: ContentsMap -> ContentsMap -> Maybe ContentsMap
 union (ContentsMap m1) (ContentsMap m2) = constructValid $ ContentsMap $ M.union m1 m2
+
+newtype DirForest =
+  DirForest
+    { dirForestMap :: Map FilePath DirOrFile
+    }
+  deriving (Show, Eq, Generic)
+
+instance Validity DirForest where
+  validate df =
+    mconcat
+      [ genericValidate df
+      , decorateList (M.toList (dirForestMap df)) $ \(fp, _) ->
+          declare "does not conain separators" $ length (FP.splitDirectories fp) == 1
+      ]
+
+data DirOrFile
+  = Dir DirForest
+  | File ByteString
+  deriving (Show, Eq, Generic)
+
+instance Validity DirOrFile
+
+makeDirForest :: Map (Path Rel File) ByteString -> Either FilePath DirForest
+makeDirForest = fmap DirForest . foldM go M.empty . M.toList
+  where
+    go ::
+         Map FilePath DirOrFile
+      -> (Path Rel File, ByteString)
+      -> Either FilePath (Map FilePath DirOrFile)
+    go m' (rp, bs) = go2 m' (FP.splitDirectories $ fromRelFile rp)
+      where
+        go2 :: Map FilePath DirOrFile -> [FilePath] -> Either FilePath (Map FilePath DirOrFile)
+        go2 m [fp] =
+          case M.lookup fp m of
+            Nothing -> pure $ M.insert fp (File bs) m
+            Just _ -> Left fp
+        go2 m (dp:rest) =
+          case M.lookup dp m of
+            Nothing -> do
+              m' <- go2 M.empty rest
+              pure $ M.insert dp (Dir $ DirForest m') m
+            Just dof ->
+              case dof of
+                Dir (DirForest df) -> go2 df rest
+                File _ -> Left dp
+
+relRoot :: Path Rel Dir
+relRoot = [reldir|.|]
