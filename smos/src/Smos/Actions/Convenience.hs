2da9a4cb132a04410399f8171d217cb97aaa70a4
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-10-18 00:24:05 +0200

    A new convenience function

--- smos/src/Smos/Actions/Convenience.hs
+++ smos/src/Smos/Actions/Convenience.hs
@@ -17,10 +17,12 @@ import Smos.Types
 import Smos.Cursor.Entry
 import Smos.Cursor.Header
 
+import Smos.Actions.Forest
+import Smos.Actions.Entry
 import Smos.Actions.Utils
 
 allConveniencePlainActions :: [Action]
-allConveniencePlainActions = [convDoneAndWaitForResponse, convRepinged]
+allConveniencePlainActions = [convDoneAndWaitForResponse, convRepinged, convNewEntryAndClockIn]
 
 convDoneAndWaitForResponse :: Action
 convDoneAndWaitForResponse =
@@ -72,3 +74,14 @@ convRepinged =
 
 insertHeaderString :: String -> SmosM ()
 insertHeaderString s = modifyHeaderCursorWhenSelectedM $ \hc -> foldM (flip headerCursorInsert) hc s
+
+convNewEntryAndClockIn :: Action
+convNewEntryAndClockIn =
+  Action
+    { actionName = "convNewEntryAndClockIn"
+    , actionFunc =
+        do actionFunc forestInsertEntryAfterAndSelectHeader
+           actionFunc entrySelectWhole
+           actionFunc forestClockOutEverywhereInAllFilesAndClockInHere
+    , actionDescription = "Create a new entry and clock in immediately"
+    }
