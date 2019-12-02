a1727aebdb12f1c39fc43f395f36875349965793
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-12-03 00:14:04 +0100

    Set up a bunch of stubs to start filling in the template rendering

--- smos-scheduler/src/Smos/Scheduler.hs
+++ smos-scheduler/src/Smos/Scheduler.hs
@@ -6,9 +6,14 @@ module Smos.Scheduler
 
 import GHC.Generics (Generic)
 
+import qualified Data.ByteString as SB
 import Data.Char as Char
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Set (Set)
 import qualified Data.Text as T
 import Data.Time
+import Data.Yaml as Yaml
 import Text.Show.Pretty
 
 import Path
@@ -35,14 +40,68 @@ handleScheduleItem wdir ScheduleItem {..} = do
   let from = wdir </> scheduleItemTemplate
   let to = wdir </> scheduleItemDestination
   pPrint (from, to)
-  templateExists <- doesFileExist from
-  if templateExists
-    then do
-      destinationExists <- doesFileExist to
-      if destinationExists
-        then putStrLn $
-             unwords ["WARNING: destination already exists:", fromAbsFile to, "not overwriting."]
-        else do
-          ensureDir $ parent to
-          copyFile from to
-    else putStrLn $ unwords ["WARNING: template does not exist:", fromAbsFile from]
+  mContents <- forgivingAbsence $ SB.readFile $ fromAbsFile from
+  case mContents of
+    Nothing -> putStrLn $ unwords ["WARNING: template does not exist:", fromAbsFile from]
+    Just contents ->
+      case Yaml.decodeEither' contents of
+        Left err ->
+          putStrLn $
+          unlines
+            [ unwords ["WARNING: Does not look like a smos template file:", fromAbsFile from]
+            , prettyPrintParseException err
+            ]
+        Right template -> do
+          pPrint template
+          let rendered = renderTemplate template
+          pPrint rendered
+          destinationExists <- doesFileExist to
+          if destinationExists
+            then putStrLn $
+                 unwords
+                   ["WARNING: destination already exists:", fromAbsFile to, "not overwriting."]
+            else do
+              ensureDir $ parent to
+              writeSmosFile to rendered
+
+renderTemplate :: ScheduleTemplate -> SmosFile
+renderTemplate (ScheduleTemplate f) = SmosFile $ map (fmap renderEntryTemplate) f
+
+renderEntryTemplate :: EntryTemplate -> Entry
+renderEntryTemplate EntryTemplate {..} =
+  Entry
+    { entryHeader = renderHeaderTemplate entryTemplateHeader
+    , entryContents = fmap renderContentsTemplate entryTemplateContents
+    , entryTimestamps = renderTimestampsTemplate entryTemplateTimestamps
+    , entryProperties = renderPropertiesTemplate entryTemplateProperties
+    , entryStateHistory = renderStateHistoryTemplate entryTemplateStateHistory
+    , entryTags = renderTagsTemplate entryTemplateTags
+    , entryLogbook = emptyLogbook
+    }
+
+renderHeaderTemplate :: Header -> Header
+renderHeaderTemplate = id -- TODO
+
+renderContentsTemplate :: Contents -> Contents
+renderContentsTemplate = id -- TODO
+
+renderTimestampsTemplate :: Map TimestampName TimestampTemplate -> Map TimestampName Timestamp
+renderTimestampsTemplate = M.map renderTimestampTemplate -- TODO
+
+renderTimestampTemplate :: TimestampTemplate -> Timestamp
+renderTimestampTemplate = undefined
+
+renderPropertiesTemplate :: Map PropertyName PropertyValue -> Map PropertyName PropertyValue
+renderPropertiesTemplate = id -- TODO
+
+renderStateHistoryTemplate :: StateHistoryTemplate -> StateHistory
+renderStateHistoryTemplate (StateHistoryTemplate es) =
+  StateHistory $ map renderStateHistoryEntryTemplate es
+
+renderStateHistoryEntryTemplate
+          :: StateHistoryEntryTemplate -> StateHistoryEntry
+renderStateHistoryEntryTemplate = undefined
+
+
+renderTagsTemplate :: Set Tag -> Set Tag
+renderTagsTemplate = id -- TODO
--- smos-scheduler/src/Smos/Scheduler/OptParse/Types.hs
+++ smos-scheduler/src/Smos/Scheduler/OptParse/Types.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -7,7 +8,17 @@ module Smos.Scheduler.OptParse.Types where
 import GHC.Generics (Generic)
 
 import Data.Aeson
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.Text (Text)
+import Data.Tree
 import Path
+
+import Control.Applicative
+
+import Smos.Data
 
 import Smos.Report.Config as Report
 import qualified Smos.Report.OptParse.Types as Report
@@ -73,3 +84,95 @@ data Settings =
     , setSchedule :: Schedule
     }
   deriving (Show, Eq)
+
+newtype ScheduleTemplate =
+  ScheduleTemplate
+    { scheduleTemplateForest :: Forest EntryTemplate
+    }
+  deriving (Show, Eq)
+
+instance FromJSON ScheduleTemplate where
+  parseJSON v = ScheduleTemplate . unForYaml <$> parseJSON v
+
+instance FromJSON (ForYaml (Tree EntryTemplate)) where
+  parseJSON v =
+    fmap ForYaml $
+    case v of
+      Object o -> do
+        mv <- o .:? "entry"
+                -- This marks that we want to be trying to parse a tree and NOT an entry.
+                -- We force the parser to make a decision this way.
+        case mv :: Maybe Value of
+          Nothing -> Node <$> parseJSON v <*> pure []
+          Just _ ->
+            (withObject "Tree Entry" $ \o' ->
+               Node <$> o .: "entry" <*> (unForYaml <$> o' .:? "forest" .!= ForYaml []))
+              v
+      _ -> Node <$> parseJSON v <*> pure []
+
+data EntryTemplate =
+  EntryTemplate
+    { entryTemplateHeader :: Header
+    , entryTemplateContents :: Maybe Contents
+    , entryTemplateTimestamps :: Map TimestampName TimestampTemplate
+    , entryTemplateProperties :: Map PropertyName PropertyValue
+    , entryTemplateStateHistory :: StateHistoryTemplate
+    , entryTemplateTags :: Set Tag
+    }
+  deriving (Show, Eq, Generic)
+
+newEntryTemplate :: Header -> EntryTemplate
+newEntryTemplate h =
+  EntryTemplate
+    { entryTemplateHeader = h
+    , entryTemplateContents = Nothing
+    , entryTemplateTimestamps = M.empty
+    , entryTemplateProperties = M.empty
+    , entryTemplateStateHistory = emptyStateHistoryTemplate
+    , entryTemplateTags = S.empty
+    }
+
+instance FromJSON EntryTemplate where
+  parseJSON v =
+    (do h <- parseJSON v
+        pure $ newEntryTemplate h) <|>
+    (withObject "EntryTemplate" $ \o ->
+       EntryTemplate <$> o .:? "header" .!= emptyHeader <*> o .:? "contents" <*>
+       o .:? "timestamps" .!= M.empty <*>
+       o .:? "properties" .!= M.empty <*>
+       o .:? "state-history" .!= emptyStateHistoryTemplate <*>
+       o .:? "tags" .!= S.empty)
+      v
+
+newtype TimestampTemplate =
+  TimestampTemplate
+    { timestampTemplateText :: Text
+    }
+  deriving (Show, Eq, Ord, Generic, FromJSON)
+
+newtype StateHistoryTemplate =
+  StateHistoryTemplate
+    { stateHistoryEntryTemplates :: [StateHistoryEntryTemplate]
+    }
+  deriving (Show, Eq, Generic, FromJSON)
+
+emptyStateHistoryTemplate :: StateHistoryTemplate
+emptyStateHistoryTemplate = StateHistoryTemplate {stateHistoryEntryTemplates = []}
+
+data StateHistoryEntryTemplate =
+  StateHistoryEntryTemplate
+    { stateHistoryEntryTemplateNewState :: Maybe TodoState
+    , stateHistoryEntryTemplateTimestamp :: UTCTimeTemplate
+    }
+  deriving (Show, Eq, Generic)
+
+instance FromJSON StateHistoryEntryTemplate where
+  parseJSON =
+    withObject "StateHistoryEntryTemplate" $ \o ->
+      StateHistoryEntryTemplate <$> o .: "new-state" <*> o .: "timestamp"
+
+newtype UTCTimeTemplate =
+  UTCTimeTemplate
+    { utcTimeTemplateText :: Text
+    }
+  deriving (Show, Eq, Ord, Generic, FromJSON)
