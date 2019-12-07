8d33864588ee79115c28f677ae9269cfe077dd9c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-12-07 12:04:52 +0100

    upgraded the nix-based pre-commit hooks

--- default.nix
+++ default.nix
@@ -2,12 +2,17 @@ let
   pkgs = import ./nix/pkgs.nix;
   nix-pre-commit-hooks =
     import (
-      builtins.fetchTarball "https://github.com/hercules-ci/nix-pre-commit-hooks/archive/a3bd860016653ab53ed49e2c4523e3e7297e58bb.tar.gz"
+      builtins.fetchTarball "https://github.com/hercules-ci/nix-pre-commit-hooks/archive/5c3078ad58856ce22f883b5518879d27bfc59dd5.tar.gz"
     );
 in
   pkgs.smosPackages // {
   "smos-docs-site" = pkgs.smosDocumentationSite;
   pre-commit-check = nix-pre-commit-hooks.run {
     src = pkgs.gitignoreSource ./.;
+    hooks = {
+      canonix.enable = true;
+      hlint.enable = true;
+      hindent.enable = true;
+    };
   };
 }
