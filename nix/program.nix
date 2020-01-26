427eb4b0fc4842b7e13bd8a3791c2ba952876857
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-01-26 22:23:21 +0100

    fixed the build

--- nix/program.nix
+++ nix/program.nix
@@ -6,64 +6,85 @@ let
   cfg = config.programs.smos;
 
 
-
 in {
-  options = {
-    programs.smos = {
-      enable = mkEnableOption "Smos cli and syncing";
-      workflowDir = mkOption {
-        type = types.str;
-        description = "Smos' workflow directory";
-        default = config.home.homeDirectory + "/workflow";
-      };
-      extraConfig = mkOption {
-        type = types.str;
-        description = "Extra contents for the config file";
-        default = "";
-      };
-      backup = mkOption {
-        default = null;
-        type = types.nullOr (types.submodule {
-          options = {
-            enable = mkEnableOption "Smos backups";
-            backupDir = mkOption {
+  options =
+    {
+      programs.smos =
+        {
+          enable = mkEnableOption "Smos cli and syncing";
+          workflowDir =
+            mkOption {
               type = types.str;
-              default = ".smos/backup";
-              description = "The directory to backup to";
+              description = "Smos' workflow directory";
+              default = config.home.homeDirectory + "/workflow";
             };
-          };
-        });
-      };
-      sync = mkOption {
-        default = null;
-        type = types.nullOr (types.submodule {
-          options = {
-            enable = mkEnableOption "Smos syncing";
-            server-url = mkOption {
+          extraConfig =
+            mkOption {
               type = types.str;
-              example = "api.smos.cs-syd.eu";
-              description = "The url of the sync server";
+              description = "Extra contents for the config file";
+              default = "";
             };
-            username = mkOption {
-              type = types.str;
-              example = "syd";
-              description = "The username to use when logging into the sync server";
+          backup =
+            mkOption {
+              default = null;
+              type =
+                types.nullOr (
+                  types.submodule {
+                    options =
+                      {
+                        enable = mkEnableOption "Smos backups";
+                        backupDir =
+                          mkOption {
+                            type = types.str;
+                            default = ".smos/backup";
+                            description = "The directory to backup to";
+                          };
+                      };
+                  }
+                );
             };
-            password = mkOption {
-              type = types.str;
-              example = "hunter12";
-              description = "The password to use when logging into the sync server";
+          sync =
+            mkOption {
+              default = null;
+              type =
+                types.nullOr (
+                  types.submodule {
+                    options =
+                      {
+                        enable = mkEnableOption "Smos syncing";
+                        server-url =
+                          mkOption {
+                            type = types.str;
+                            example = "api.smos.cs-syd.eu";
+                            description = "The url of the sync server";
+                          };
+                        username =
+                          mkOption {
+                            type = types.str;
+                            example = "syd";
+                            description =
+                              "The username to use when logging into the sync server";
+                          };
+                        password =
+                          mkOption {
+                            type = types.str;
+                            example = "hunter12";
+                            description =
+                              "The password to use when logging into the sync server";
+                          };
+                      };
+                  }
+                );
             };
-          };
-        });
-      };
+        };
     };
-  };
   config =
     let
       smosPkgs = (import ./pkgs.nix).smosPackages;
 
-      syncConfigContents = syncCfg: optionalString (syncCfg.enable or false) ''
+      syncConfigContents =
+        syncCfg:
+          optionalString ( syncCfg.enable or false ) ''
 
 sync:
   server-url: "${cfg.sync.server-url}"
@@ -72,18 +93,23 @@ sync:
 
     '';
 
-      smosConfigContents = concatStringsSep "\n" [
-        (syncConfigContents cfg.sync)
-        cfg.extraConfig
+      smosConfigContents =
+        concatStringsSep "\n" [
+          ( syncConfigContents cfg.sync )
+          cfg.extraConfig
         ];
 
       backupSmosName = "backup-smos";
-      backupSmosService = {
-        Unit = {
-          Description = "Backup smos";
-        };
-        Service = {
-          ExecStart = "${pkgs.writeShellScript "backup-smos-service-ExecStart"
+      backupSmosService =
+        {
+          Unit =
+            {
+              Description = "Backup smos";
+            };
+          Service =
+            {
+              ExecStart =
+                "${pkgs.writeShellScript "backup-smos-service-ExecStart"
             ''
               export PATH="$PATH:${pkgs.coreutils}/bin"
               set -ex
@@ -92,86 +118,103 @@ sync:
               mkdir -p "$subdir"
               exec ${pkgs.rsync}/bin/rsync --recursive ${cfg.workflowDir}/ "$subdir"
             ''}";
-          Type = "oneshot";
+              Type = "oneshot";
+            };
         };
-      };
-      backupSmosTimer = {
-        Unit = {
-          Description = "Backup smos every day";
+      backupSmosTimer =
+        {
+          Unit =
+            {
+              Description = "Backup smos every day";
+            };
+          Install =
+            {
+              WantedBy = [ "timers.target" ];
+            };
+          Timer =
+            {
+              OnCalendar = "*-*-* 00:00";
+              Persistent = true;
+              Unit = "${backupSmosName}.service";
+            };
         };
-        Install = {
-          WantedBy = [ "timers.target" ];
-        };
-        Timer = {
-          OnCalendar = "*-*-* 00:00";
-          Persistent = true;
-          Unit = "${backupSmosName}.service";
-        };
-      };
 
       syncSmosName = "sync-smos";
-      syncSmosService = {
-        Unit = {
-          Description = "Sync smos";
-          Wants = [ "network-online.target" ];
-        };
-        Service = {
-          ExecStart = "${pkgs.writeShellScript "sync-smos-service-ExecStart" 
+      syncSmosService =
+        {
+          Unit =
+            {
+              Description = "Sync smos";
+              Wants = [ "network-online.target" ];
+            };
+          Service =
+            {
+              ExecStart =
+                "${pkgs.writeShellScript "sync-smos-service-ExecStart" 
             ''
               exec ${smosPkgs.smos-sync-client}/bin/smos-sync-client sync
             ''}";
-          Type = "oneshot";
+              Type = "oneshot";
+            };
         };
-      };
-      syncSmosTimer = {
-        Unit = {
-          Description = "Sync smos every five minutes for";
+      syncSmosTimer =
+        {
+          Unit =
+            {
+              Description = "Sync smos every five minutes for";
+            };
+          Install =
+            {
+              WantedBy = [ "timers.target" ];
+            };
+          Timer =
+            {
+              OnCalendar = "*:0/5";
+              Persistent = true;
+              Unit = "${syncSmosName}.service";
+            };
         };
-        Install = {
-          WantedBy = [ "timers.target" ];
-        };
-        Timer = {
-          OnCalendar = "*:0/5";
-          Persistent = true;
-          Unit = "${syncSmosName}.service";
-        };
-      };
-      
-      services = (
-        optionalAttrs (cfg.sync.enable or false) {
+
+      services =
+        (
+          optionalAttrs (cfg.sync.enable or false) {
           "${syncSmosName}" = syncSmosService;
         }
         //
         optionalAttrs (cfg.backup.enable or false) {
           "${backupSmosName}" = backupSmosService;
         }
-      );
-      timers = (
-        optionalAttrs (cfg.sync.enable or false) {
+        );
+      timers =
+        (
+          optionalAttrs (cfg.sync.enable or false) {
           "${syncSmosName}" = syncSmosTimer;
         }
         //
         optionalAttrs (cfg.backup.enable or false) {
           "${backupSmosName}" = backupSmosTimer;
         }
-      );
-      packages = [
-        smosPkgs.smos
-        smosPkgs.smos-archive
-        smosPkgs.smos-convert-org
-        smosPkgs.smos-single
-        smosPkgs.smos-query
-        smosPkgs.smos-sync-client
-      ];
-        
-        
-    in mkIf cfg.enable {
-      xdg.configFile."smos/config.yaml".text = smosConfigContents;
-      systemd.user = {
-        startServices = true;
-        services = services;
-        timers = timers;
+        );
+      packages =
+        [
+          smosPkgs.smos
+          smosPkgs.smos-archive
+          smosPkgs.smos-convert-org
+          smosPkgs.smos-single
+          smosPkgs.smos-query
+          smosPkgs.smos-sync-client
+        ];
+
+
+    in
+      mkIf cfg.enable {
+        xdg.configFile."smos/config.yaml".text = smosConfigContents;
+        systemd.user =
+          {
+            startServices = true;
+            services = services;
+            timers = timers;
+          };
+        home.packages = packages;
       };
-      home.packages = packages;
-    };
 }
