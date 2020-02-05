2c202cd8fd009f9a8060dfdb947dbbcef5704136
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-02-05 02:05:06 +0100

    trying out the latest fixed size generators

--- nix/validity-version.nix
+++ nix/validity-version.nix
@@ -1,6 +1,6 @@
 {
   owner = "NorfairKing";
   repo = "validity";
-  rev = "738050966d6f10b63a47c7f93d692d0efeee72d8";
-  sha256 = "sha256:0pxp46b6a3mk4wrpnljv6nadg5jqkkk4iwxam65s726czy90yhcc";
+  rev = "513a6d8353ff4f01adda4882b92794f518a364ba";
+  sha256 = "sha256:0qprhd573v0m4vwq74i2b6i0nlzlvsff06sixvx5c5q5zj7x0dpy";
 }
