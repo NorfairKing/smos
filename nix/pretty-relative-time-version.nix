a8036878f2963bf86f678b5e17d2d101360f7f4d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-02-10 15:24:33 +0100

    upgraded pretty-relative-time

--- nix/pretty-relative-time-version.nix
+++ nix/pretty-relative-time-version.nix
@@ -1,6 +1,6 @@
 {
   owner = "NorfairKing";
   repo = "pretty-relative-time";
-  rev = "8662d4c999d27e6723aa4f4013ecf08b9f92b697";
-  sha256 = "sha256:1bb4n9vg8zbh9k4nm43mhknn74v2m7dxlzml9jj13janc8gdmxlm";
+  rev = "fc62907b36f6f7348d6ae0e97aceb4d41039d57b";
+  sha256 = "sha256:0dalpi3kwlr7m14sjjf2jvxfq8ydskarra2gc5j4w134m4r1m2ca";
 }
