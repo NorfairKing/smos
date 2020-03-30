1e2797d69527de3db89da6750edc47cddb51e34e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-03-30 21:37:24 +0200

    upgraded lts

--- nix/nixpkgs-version.nix
+++ nix/nixpkgs-version.nix
@@ -1,4 +1,4 @@
 {
-  rev = "323fe92057bb38475a446ff5e18d3b388c2e4403";
-  sha256 = "sha256:0hmjasnq0x40i2x22d4qrqf1hph4bwrjhx4zwz2s6nyqfqinbcyn";
+  rev = "a87b506140a7267477103759c3f8da5b2e8d994e";
+  sha256 = "sha256:0h8i0b2dyz8bppkfs5srkaq4hrc9ngb6idg7cq9ngvz3szr6xx08";
 }
