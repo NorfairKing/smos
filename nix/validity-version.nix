fa97a3d25a73dfd179302bc8447ffd441d2be50e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-04-15 01:42:29 +0200

    Use faster bytestring generator from newer validity

--- nix/validity-version.nix
+++ nix/validity-version.nix
@@ -1,6 +1,6 @@
 {
   owner = "NorfairKing";
   repo = "validity";
-  rev = "0ddfd121ed48df95e1f5f9dd6ce99ca82130b53c";
-  sha256 = "sha256:0xza57csinjyax75z82rc6dbm0d6p3p0dxs7qvdjnf2rqmbxhxk4";
+  rev = "3d7cdbcec808b8249918692adcad187d3cf2e8f4";
+  sha256 = "sha256:0hnlglzsvx2v5wc0z7njq9mf47jzs9jr2sskzz9rix4h2grzx0jm";
 }
