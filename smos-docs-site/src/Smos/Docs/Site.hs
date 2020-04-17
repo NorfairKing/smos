1f90867cdcf457c07d6602c39df155ec26a099bf
Walter Franzini <walter.franzini@gmail.com>
2020-04-17 22:37:37 +0200

    Adds command reference and keybinding reference pages (autogenerated)
    The generated pages are partial to gather feedback about the optimal
    solution.

--- smos-docs-site/src/Smos/Docs/Site.hs
+++ smos-docs-site/src/Smos/Docs/Site.hs
@@ -5,11 +5,22 @@ module Smos.Docs.Site
   )
 where
 
-import Hakyll
-import Hakyll.Web.Sass
+import           Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import           Hakyll
+import           Hakyll.Web.Sass
+import           Smos.Actions
+import           Smos.Default
+import           Smos.Keys
+import           Smos.Types
+import           System.Directory
+import           System.IO
 
 smosDocsSite :: IO ()
-smosDocsSite =
+smosDocsSite = do
+  createDirectoryIfMissing True "_generated"
+  generateKeybindingDocs "Help"
   hakyll $ do
     match "assets/*" $ do
       route idRoute
@@ -19,6 +30,12 @@ smosDocsSite =
       compile (sassCompilerWith sassDefConfig {sassIncludePaths = Just ["_sass"]})
     match "pages/*" $ do
       route $ composeRoutes (gsubRoute "pages/" (const "")) (setExtension "html")
+      compile $
+        pandocCompiler >>= loadAndApplyTemplate "templates/page.html" pageCtx
+          >>= loadAndApplyTemplate "templates/default.html" pageCtx
+          >>= relativizeUrls
+    match "_generated/*" $ do
+      route $ composeRoutes (gsubRoute "_generated/" (const "")) (setExtension "html")
       compile $
         pandocCompiler >>= loadAndApplyTemplate "templates/page.html" pageCtx
           >>= loadAndApplyTemplate "templates/default.html" pageCtx
@@ -37,3 +54,31 @@ smosDocsSite =
 
 pageCtx :: Context String
 pageCtx = defaultContext
+
+
+renderKeyMapping :: KeyMapping -> (Text, Text, Text)
+renderKeyMapping keyMapping =
+  case keyMapping of
+    (MapVtyExactly kp act) -> (renderKeyPress kp, actionNameText $ actionName act, actionDescription act)
+    (MapAnyTypeableChar au) -> ("??", actionNameText $ actionUsingName au, actionUsingDescription au)
+    (MapCatchAll act) -> ("any char", actionNameText $ actionName act, actionDescription act)
+    (MapCombination kp km) ->
+      let (kp', cmd, desc) =  renderKeyMapping km
+      in (renderKeyPress kp <> kp', cmd, desc)
+
+generateKeybindingDocs :: Text -> IO ()
+generateKeybindingDocs label =
+  withFile "_generated/keybindings.markdown" WriteMode $ \h -> do
+  T.hPutStrLn h "---"
+  T.hPutStrLn h "title: Key Bindings"
+  T.hPutStrLn h "---"
+  T.hPutStrLn h ""
+  T.hPutStrLn h $ "## " <> label
+  T.hPutStrLn h "Key | Command | Description"
+  T.hPutStrLn h "---:+:-------:+:-----------:"
+  forM_ (keyMapHelpMatchers $ configKeyMap defaultConfig) $ \keyMapping ->
+      T.hPutStrLn h $ renderRow $ renderKeyMapping keyMapping
+
+  where
+    renderRow :: (Text, Text, Text) -> Text
+    renderRow (kb, cmd, desc) = (T.pack $ escapeHtml $ T.unpack kb) <> " | " <> cmd <> " | " <> desc
