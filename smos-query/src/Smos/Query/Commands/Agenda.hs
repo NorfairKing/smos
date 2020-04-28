7871338dd962e2d6876b32cc4234fb28e4207a5b
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-04-28 12:28:46 +0200

    Refactoring of the agenda command

--- smos-query/src/Smos/Query/Commands/Agenda.hs
+++ smos-query/src/Smos/Query/Commands/Agenda.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -12,6 +14,7 @@ import qualified Data.Conduit.Combinators as C
 import Data.List
 import Data.Text (Text)
 import Data.Time
+import GHC.Generics
 import Rainbow
 import Smos.Query.Config
 import Smos.Query.Formatting
@@ -35,18 +38,34 @@ smosQueryAgenda AgendaSettings {..} = do
   liftIO $ putTableLn $ renderAgendaReport now $ makeAgendaReport now agendaSetBlock tups
 
 renderAgendaReport :: ZonedTime -> AgendaReport -> Table
-renderAgendaReport now AgendaReport {..} =
-  formatAsTable
-    $ intercalate [[chunk ""]]
-    $ filter
+renderAgendaReport now = formatAsTable . renderAgendaReportLines now . makeAgendaReportLines
+
+renderAgendaReportLines :: ZonedTime -> [AgendaReportLine] -> [[Chunk Text]]
+renderAgendaReportLines now = map $ \case
+  TitleLine t -> [fore blue $ chunk t]
+  SpaceLine -> [chunk ""]
+  EntryLine ae -> formatAgendaEntry now ae
+
+data AgendaReportLine
+  = TitleLine Text
+  | SpaceLine
+  | EntryLine AgendaEntry
+  deriving (Show, Eq, Generic)
+
+makeAgendaReportLines :: AgendaReport -> [AgendaReportLine]
+makeAgendaReportLines AgendaReport {..} =
+  concat $
+    filter
       (not . null)
       [goBlocks agendaReportPast, goBlocks agendaReportPresent, goBlocks agendaReportFuture]
   where
+    goBlocks :: [AgendaTableBlock Text] -> [AgendaReportLine]
     goBlocks bs =
       case bs of
         [] -> []
         [b] -> goEntries $ blockEntries b
         _ -> concatMap goBlock bs
-    goBlock Block {..} = [fore blue $ chunk blockTitle] : goEntries blockEntries
-    goEntries :: [AgendaEntry] -> [[Chunk Text]]
-    goEntries = map (formatAgendaEntry now)
+    goBlock :: AgendaTableBlock Text -> [AgendaReportLine]
+    goBlock Block {..} = TitleLine blockTitle : goEntries blockEntries
+    goEntries :: [AgendaEntry] -> [AgendaReportLine]
+    goEntries = map EntryLine
