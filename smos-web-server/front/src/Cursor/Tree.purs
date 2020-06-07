87bc4becd96faed0fa9d5e618d07a0c0a366fd3b
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-06-07 18:06:48 +0200

    Translated Cursor.Tree

---
+++ smos-web-server/front/src/Cursor/Tree.purs
@@ -0,0 +1,21 @@
+module Cursor.Tree
+  ( module Cursor.Tree.Types
+  , module Cursor.Tree.Base
+  , module Cursor.Tree.Collapse
+  , module Cursor.Tree.Movement
+  , module Cursor.Tree.Insert
+  , module Cursor.Tree.Delete
+  , module Cursor.Tree.Swap
+  , module Cursor.Tree.Promote
+  , module Cursor.Tree.Demote
+  ) where
+
+import Cursor.Tree.Base
+import Cursor.Tree.Collapse
+import Cursor.Tree.Delete
+import Cursor.Tree.Demote
+import Cursor.Tree.Insert
+import Cursor.Tree.Movement
+import Cursor.Tree.Promote
+import Cursor.Tree.Swap
+import Cursor.Tree.Types
