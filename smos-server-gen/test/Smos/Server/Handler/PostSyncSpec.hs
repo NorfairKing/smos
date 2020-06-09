bd354a86781fd6b662babc12c6d9b147824b4ec8
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-06-09 09:37:07 +0200

    regression test for a sync bug

--- smos-server-gen/test/Smos/Server/Handler/PostSyncSpec.hs
+++ smos-server-gen/test/Smos/Server/Handler/PostSyncSpec.hs
@@ -5,6 +5,7 @@ module Smos.Server.Handler.PostSyncSpec
   )
 where
 
+import Data.Mergeful
 import Smos.Client
 import Smos.Server.TestUtils
 import Test.Hspec
@@ -12,11 +13,18 @@ import Test.Validity
 
 spec :: Spec
 spec =
-  serverSpec
-    $ describe "PostSync"
-    $ it "produces valid resuls"
-    $ \cenv ->
+  serverSpec $ describe "PostSync" $ do
+    it "produces valid resuls" $ \cenv ->
       forAllValid $ \request ->
         withNewUser cenv $ \t -> do
           response <- testClientOrErr cenv (clientPostSync t request)
           shouldBeValid response
+    it "is idempotent after an arbitrary setup request" $ \cenv ->
+      forAllValid $ \initial ->
+        forAllValid $ \request ->
+          withNewUser cenv $ \t -> do
+            let initialRequest = initialSyncRequest {syncRequestNewItems = initial}
+            _ <- testClientOrErr cenv (clientPostSync t initialRequest)
+            response1 <- testClientOrErr cenv (clientPostSync t request)
+            response2 <- testClientOrErr cenv (clientPostSync t request)
+            response2 `shouldBe` response1
