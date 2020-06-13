54d39a2d757f6caa381fb606e433a61da1ab1527
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-06-13 19:14:30 +0200

    web server: agenda report

--- demo-workflow/shell.nix
+++ demo-workflow/shell.nix
@@ -15,6 +15,15 @@ pkgs.stdenv.mkDerivation rec {
   shellHook = ''
     export SMOS_WORKFLOW_DIR=$(pwd)
     export SMOS_CONFIG_FILE=$(pwd)/config.yaml
+
+    # Sync setup
+    export SMOS_CONTENTS_DIR=$(pwd)
+    export SMOS_USERNAME=exampleuser
+    export SMOS_PASSWORD=examplepass
+    export SMOS_SERVER_URL=localhost:8001
+    export SMOS_UUID_FILE=$(pwd)/../server.uuid
+    export SMOS_SESSION_PATH=$(pwd)/../session.json
+    export SMOS_METADATA_DATABASE=$(pwd)/../sync-metadata.sqlite3
   '';
 
 }
