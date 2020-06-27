e9538ab74596307205bd17930d476ad4560f9508
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-06-27 03:43:38 +0200

    rendering the templating language

--- smos-scheduler/src/Smos/Scheduler/Template.hs
+++ smos-scheduler/src/Smos/Scheduler/Template.hs
@@ -1,22 +1,36 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Smos.Scheduler.Template where
 
 import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Validity
 import Data.Validity.Text ()
 import GHC.Generics (Generic)
 
-data Template
-  = TLit Text -- "Literal text"
-  | TTime Text -- "[%F]", translates to 2020-06-27
-  | TRelTime Text -- "[%F|tomorrow]", translates to 2020-06-28
+newtype Template = Template {templatePieces :: [TemplatePiece]}
   deriving (Show, Eq, Generic)
 
 instance Validity Template
+
+data TemplatePiece
+  = TLit Text -- "Literal text", translates to "Literal text"
+  | TTime Text -- "[ %F ]", translates to "2020-06-27"
+  | TRelTime Text Text -- "[ %F | tomorrow ]", translates to "2020-06-28"
+  deriving (Show, Eq, Generic)
+
+instance Validity TemplatePiece
 
 parseTemplate :: Text -> Either String Template
 parseTemplate = undefined
 
 renderTemplate :: Template -> Text
-renderTemplate = undefined
+renderTemplate = T.concat . map renderTemplatePiece . templatePieces
+
+renderTemplatePiece :: TemplatePiece -> Text
+renderTemplatePiece = \case
+  TLit t -> t
+  TTime t -> "[ " <> t <> " ]"
+  TRelTime t1 t2 -> "[ " <> t1 <> " | " <> t2 <> " ]"
--- smos-scheduler/test/Smos/Scheduler/TemplateSpec.hs
+++ smos-scheduler/test/Smos/Scheduler/TemplateSpec.hs
@@ -16,9 +16,15 @@ instance GenValid Template where
   genValid = genValidStructurally
   shrinkValid = shrinkValidStructurally
 
+instance GenValid TemplatePiece where
+  genValid = genValidStructurally
+  shrinkValid = shrinkValidStructurally
+
 spec :: Spec
 spec = do
+  genValidSpec @TemplatePiece
   genValidSpec @Template
   describe "parseTemplate" $ it "parses into valid values" $ producesValidsOnValids parseTemplate
+  describe "renderTemplatePiece" $ it "produces valids values" $ producesValidsOnValids renderTemplatePiece
   describe "renderTemplate" $ it "produces valids values" $ producesValidsOnValids renderTemplate
   describe "renderTemplate and parseTemplate" $ it "are inverses" $ forAllValid $ \t -> parseTemplate (renderTemplate t) `shouldBe` Right t
