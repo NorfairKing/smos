c6c5e111dec48e9c78c6de92554f38ac9713e427
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-06-27 04:03:00 +0200

    some amount of parsing

--- smos-scheduler/src/Smos/Scheduler/Template.hs
+++ smos-scheduler/src/Smos/Scheduler/Template.hs
@@ -16,15 +16,23 @@ newtype Template = Template {templatePieces :: [TemplatePiece]}
 instance Validity Template
 
 data TemplatePiece
-  = TLit Text -- "Literal text", translates to "Literal text"
-  | TTime Text -- "[ %F ]", translates to "2020-06-27"
-  | TRelTime Text Text -- "[ %F | tomorrow ]", translates to "2020-06-28"
+  = -- | "Literal text", translates to "Literal text"
+    TLit Text
+  | -- | A time formatting string
+    --
+    -- All leading and trailing whitespace is skipped
+    --
+    -- Example: "[ %F ]", translates to "2020-06-27"
+    TTime Text
+  | -- |  A relative time formatting string
+    --
+    -- All leading and trailing whitespace is skipped
+    --
+    -- "[ %F | tomorrow ]", translates to "2020-06-28"
+    TRelTime Text Text
   deriving (Show, Eq, Generic)
 
 instance Validity TemplatePiece
-
-parseTemplate :: Text -> Either String Template
-parseTemplate = undefined
 
 renderTemplate :: Template -> Text
 renderTemplate = T.concat . map renderTemplatePiece . templatePieces
@@ -34,3 +42,25 @@ renderTemplatePiece = \case
   TLit t -> t
   TTime t -> "[ " <> t <> " ]"
   TRelTime t1 t2 -> "[ " <> t1 <> " | " <> t2 <> " ]"
+
+parseTemplate :: Text -> Either String Template
+parseTemplate = fmap Template . go . T.unpack
+  where
+    go :: String -> Either String [TemplatePiece]
+    go cs = case break (== '[') cs of
+      (l, cs') -> do
+        let h = [TLit (T.pack l) | not (null l)]
+        t <-
+          if null cs'
+            then pure [] -- the string does not contain '['
+            else goVar (drop 1 cs') -- The first character is '['
+        pure $ h ++ t
+    goVar :: String -> Either String [TemplatePiece]
+    goVar cs = case break (== ']') cs of
+      (l, cs') -> do
+        let h = [TTime (T.strip (T.pack l))]
+        t <-
+          if null cs'
+            then Left "Unmatched [" -- the string does not contain ']'
+            else go (drop 1 cs') -- The first character is ']'
+        pure $ h ++ t
--- smos-scheduler/test/Smos/Scheduler/TemplateSpec.hs
+++ smos-scheduler/test/Smos/Scheduler/TemplateSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
@@ -24,7 +25,23 @@ spec :: Spec
 spec = do
   genValidSpec @TemplatePiece
   genValidSpec @Template
-  describe "parseTemplate" $ it "parses into valid values" $ producesValidsOnValids parseTemplate
   describe "renderTemplatePiece" $ it "produces valids values" $ producesValidsOnValids renderTemplatePiece
   describe "renderTemplate" $ it "produces valids values" $ producesValidsOnValids renderTemplate
   describe "renderTemplate and parseTemplate" $ it "are inverses" $ forAllValid $ \t -> parseTemplate (renderTemplate t) `shouldBe` Right t
+  describe "parseTemplate" $ do
+    it "parses into valid values" $ producesValidsOnValids parseTemplate
+    let s t ps =
+          let r = Template ps
+           in it ("succesfully parses " <> show t <> " into " <> show r) $ parseTemplate t `shouldBe` Right r
+    let f t = it ("correctly fails to parse " <> show t) $ case parseTemplate t of
+          Left _ -> pure ()
+          Right r -> expectationFailure $ "Should have failed to parse, but succeeded and parsed " <> show r
+    s "" []
+    s "hello" [TLit "hello"]
+    s "hello world" [TLit "hello world"]
+    s "hello[world]" [TLit "hello", TTime "world"]
+    s "hello[ world ]" [TLit "hello", TTime "world"]
+    s "[]" [TTime ""]
+    f "hello["
+    f "hello[world"
+    f "hello[ world"
