808de8723989d7457520e35750d7f73fc855585d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-07-13 14:00:08 +0200

    sha for the pre commit hooks

--- default.nix
+++ default.nix
@@ -1,9 +1,11 @@
 let
   pkgs = import ./nix/pkgs.nix;
-  nix-pre-commit-hooks =
-    import (
-      builtins.fetchTarball "https://github.com/hercules-ci/nix-pre-commit-hooks/archive/f709c4652d4696dbe7c6a8354ebd5938f2bf807b.tar.gz"
-    );
+  nix-pre-commit-hooks = import (
+    builtins.fetchTarball {
+      url = "https://github.com/hercules-ci/nix-pre-commit-hooks/archive/f709c4652d4696dbe7c6a8354ebd5938f2bf807b.tar.gz";
+      sha256 = "sha256:0700c5awc2gjzgikhx69vjbpyshx6b5xljmpxrdzpgqyg3blxbkl";
+    }
+  );
 in
 pkgs.smosPackages // {
   release = pkgs.smosRelease;
