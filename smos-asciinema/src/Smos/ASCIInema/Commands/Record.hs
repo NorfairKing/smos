982d253c69c9d4e6045674b8805b596a417921d1
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-07-15 00:51:51 +0200

    asciicast for waiting command

--- smos-asciinema/src/Smos/ASCIInema/Commands/Record.hs
+++ smos-asciinema/src/Smos/ASCIInema/Commands/Record.hs
@@ -29,42 +29,15 @@ record rs@RecordSettings {..} = do
   mSpec <- readConfigFile recordSetSpecFile
   case mSpec of
     Nothing -> die $ "File does not exist: " <> fromAbsFile recordSetSpecFile
-    Just s -> do
-      withCurrentDir (parent recordSetSpecFile)
-        $ bracket
-          (getFileStati (asciinemaFiles s))
-          restoreFiles
-        $ \_ -> runASCIInema rs s
-
-data FileStatus
-  = FileDoesNotExist
-  | FileWithContents ByteString
-  deriving (Show, Eq)
-
-getFileStati :: [FilePath] -> IO [(Path Abs File, FileStatus)]
-getFileStati = mapM $ \f -> do
-  p <- resolveFile' f
-  s <- getFileStatus p
-  pure (p, s)
-
-getFileStatus :: Path Abs File -> IO FileStatus
-getFileStatus p = maybe FileDoesNotExist FileWithContents <$> forgivingAbsence (SB.readFile (fromAbsFile p))
-
-restoreFiles :: [(Path Abs File, FileStatus)] -> IO ()
-restoreFiles = mapM_ (uncurry restoreFile)
-
-restoreFile :: Path Abs File -> FileStatus -> IO ()
-restoreFile p = \case
-  FileDoesNotExist -> ignoringAbsence $ removeFile p
-  FileWithContents bs -> do
-    ensureDir $ parent p
-    SB.writeFile (fromAbsFile p) bs
+    Just s -> runASCIInema rs recordSetSpecFile s
 
 data ASCIInemaSpec
   = ASCIInemaSpec
       { asciinemaCommand :: Maybe String,
         asciinemaTimeout :: Int, -- Seconds
         asciinemaFiles :: [FilePath],
+        asciinemaWorkingDir :: Maybe FilePath,
+        asciinemaWorkflowDir :: Maybe FilePath,
         asciinemaInput :: [ASCIInemaCommand]
       }
   deriving (Show, Eq)
@@ -82,37 +55,89 @@ instance YamlSchema ASCIInemaSpec where
           [ (: []) <$> requiredField "file" "The file that is being touched. It will be brought back in order afterwards.",
             optionalFieldWithDefault "files" [] "The files that are being touched. These will be brought back in order afterwards."
           ]
+        <*> optionalField "working-dir" "The working directory directory"
+        <*> optionalField "workflow-dir" "The workflow directory to set via an environment variable"
         <*> optionalFieldWithDefault "input" [] "The inputs to send to the command"
 
-runASCIInema :: RecordSettings -> ASCIInemaSpec -> IO ()
-runASCIInema RecordSettings {..} ASCIInemaSpec {..} = do
-  -- Get the output file's parent directory ready
-  ensureDir $ parent recordSetOutputFile
-  env <- getEnvironment
-  let env' =
-        env
-          ++ [ ("ASCIINEMA_CONFIG_HOME", ".config")
-             ]
-  let apc =
-        setEnv env'
-          $ setStdin createPipe
-          $ proc "asciinema"
-          $ concat
-            [ ["rec", "--stdin", "--yes", "--quiet", "--overwrite", fromAbsFile recordSetOutputFile],
-              maybe [] (\c -> ["--command", c]) asciinemaCommand
-            ]
-  withProcessWait apc $ \p -> do
-    mExitedNormally <- timeout (asciinemaTimeout * 1000 * 1000) $ do
-      let h = getStdin p
-      hSetBuffering h NoBuffering
-      sendAsciinemaCommand recordSetWait h $ Wait 1
-      mapM_ (sendAsciinemaCommand recordSetWait h) asciinemaInput
-      when (isNothing asciinemaCommand) $ hPutStr h "exit\n"
-    case mExitedNormally of
-      Nothing -> do
-        stopProcess p
-        die "Asciinema got stuck for 60 seconds"
-      Just () -> pure ()
+withRestoredFiles :: [Path Abs File] -> IO a -> IO a
+withRestoredFiles fs func =
+  bracket
+    (getFileStati fs)
+    restoreFiles
+    $ const func
+
+data FileStatus
+  = FileDoesNotExist
+  | FileWithContents ByteString
+  deriving (Show, Eq)
+
+getFileStati :: [Path Abs File] -> IO [(Path Abs File, FileStatus)]
+getFileStati = mapM $ \p -> do
+  s <- getFileStatus p
+  pure (p, s)
+
+getFileStatus :: Path Abs File -> IO FileStatus
+getFileStatus p = maybe FileDoesNotExist FileWithContents <$> forgivingAbsence (SB.readFile (fromAbsFile p))
+
+restoreFiles :: [(Path Abs File, FileStatus)] -> IO ()
+restoreFiles = mapM_ (uncurry restoreFile)
+
+restoreFile :: Path Abs File -> FileStatus -> IO ()
+restoreFile p = \case
+  FileDoesNotExist -> ignoringAbsence $ removeFile p
+  FileWithContents bs -> do
+    ensureDir $ parent p
+    SB.writeFile (fromAbsFile p) bs
+
+runASCIInema :: RecordSettings -> Path Abs File -> ASCIInemaSpec -> IO ()
+runASCIInema RecordSettings {..} specFilePath ASCIInemaSpec {..} = do
+  let parentDir = parent specFilePath
+  mWorkingDir <- mapM (resolveDir parentDir) asciinemaWorkingDir
+  let dirToResolveFiles = fromMaybe parentDir mWorkingDir
+  fs <- mapM (resolveFile dirToResolveFiles) asciinemaFiles
+  withRestoredFiles fs
+    $ withCurrentDir parentDir
+    $ do
+      -- Get the output file's parent directory ready
+      env <- getEnvironment
+      mWorkflowDir <- mapM (resolveDir parentDir) asciinemaWorkflowDir
+      let env' =
+            concat
+              [ env,
+                [ ("ASCIINEMA_CONFIG_HOME", ".config")
+                ],
+                [("SMOS_WORKFLOW_DIR", fromAbsDir p) | p <- maybeToList mWorkflowDir]
+              ]
+      let apc =
+            maybe id (setWorkingDir . fromAbsDir) mWorkingDir
+              $ setEnv env'
+              $ setStdin createPipe
+              $ proc "asciinema"
+              $ concat
+                [ [ "rec",
+                    "--stdin",
+                    "--yes",
+                    "--quiet",
+                    "--overwrite",
+                    fromAbsFile recordSetOutputFile,
+                    "--env=SMOS_WORKFLOW_DIR"
+                  ],
+                  maybe [] (\c -> ["--command", c]) asciinemaCommand
+                ]
+      -- Make sure the output file can be created nicely
+      ensureDir $ parent recordSetOutputFile
+      withProcessWait apc $ \p -> do
+        mExitedNormally <- timeout (asciinemaTimeout * 1000 * 1000) $ do
+          let h = getStdin p
+          hSetBuffering h NoBuffering
+          sendAsciinemaCommand recordSetWait h $ Wait 1
+          mapM_ (sendAsciinemaCommand recordSetWait h) asciinemaInput
+          when (isNothing asciinemaCommand) $ hPutStr h "exit\n"
+        case mExitedNormally of
+          Nothing -> do
+            stopProcess p
+            die "Asciinema got stuck for 60 seconds"
+          Just () -> pure ()
 
 data ASCIInemaCommand
   = Wait Int -- Milliseconds
