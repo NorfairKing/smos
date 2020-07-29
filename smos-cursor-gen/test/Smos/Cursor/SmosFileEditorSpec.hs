8f1bf85df297dcac6eec900606d2ec6d5d4bb6de
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-07-29 14:55:51 +0200

    got tests to pass again

--- smos-cursor-gen/test/Smos/Cursor/SmosFileEditorSpec.hs
+++ smos-cursor-gen/test/Smos/Cursor/SmosFileEditorSpec.hs
@@ -17,7 +17,7 @@ import Test.Validity.Optics
 
 spec :: Spec
 spec = modifyMaxShrinks (const 1) $ do
-  describe "makeSmosFileCursor" $ it "works on any valid smos file" $ forAllValid $ \sf ->
+  describe "startSmosFileCursor" $ it "works on any valid smos file" $ forAllValid $ \sf ->
     forAllValid $ \rp ->
       withSystemTempDir "smos-cursor-test" $ \tdir -> do
         let p = tdir </> rp
@@ -27,3 +27,19 @@ spec = modifyMaxShrinks (const 1) $ do
           Nothing -> expectationFailure "Locking should have been possible"
           Just (Left err) -> expectationFailure err
           Just (Right SmosFileEditorCursor {..}) -> smosFileEditorPath `shouldBe` p
+  describe "saveSmosFile" $ do
+    it "Does not create a file if a nonexistent file is not changed"
+      $ withSystemTempDir "smos-test"
+      $ \d -> do
+        p <- resolveFile d "test.smos"
+        saveSmosFile emptySmosFile Nothing p
+        b' <- doesFileExist p
+        b' `shouldBe` False
+    it "Does not create a file if an empty file is not changed and did not exist yet"
+      $ withSystemTempDir "smos-test"
+      $ \d -> do
+        p <- resolveFile d "test.smos"
+        writeFile (toFilePath p) mempty
+        saveSmosFile emptySmosFile Nothing p
+        b' <- doesFileExist p
+        b' `shouldBe` False
--- smos/test/Smos/Cursor/EditorSpec.hs
+++ smos/test/Smos/Cursor/EditorSpec.hs
@@ -1,35 +1,25 @@
-{-# LANGUAGE TypeApplications #-}
-
 module Smos.Cursor.EditorSpec where
 
-import Smos.Cursor.Editor.Gen ()
+import Data.GenValidity.Path ()
+import Path
+import Path.IO
+import Smos.Data
 import Smos.Data.Gen ()
 import Smos.Types
 import Test.Hspec
+import Test.Hspec.QuickCheck
 import Test.Validity
-import Test.Validity.Optics
 
 spec :: Spec
-spec = do
-  genValidSpec @EditorCursor
-  describe "makeEditorCursor"
-    $ it "produces valid cursors"
-    $ producesValidsOnValids2 makeEditorCursor
-  describe "rebuildEditorCursor" $ do
-    it "produces valid cursors" $ producesValidsOnValids rebuildEditorCursor
-    it "roundtrips after makeEditorCursor" $ forAllValid $ \p -> forAllValid $ \msf ->
-      let sfec = makeEditorCursor p msf
-          (p', msf') = rebuildEditorCursor sfec
-       in (p, msf) `shouldBe` (p', msf')
-  describe "editorCursorSmosFileEditorCursorL" $ lensSpecOnValid editorCursorSmosFileEditorCursorL
-  describe "editorCursorSelectionL" $ lensSpecOnValid editorCursorSelectionL
-  describe "editorCursorDebugL" $ lensSpecOnValid editorCursorDebugL
-  describe "editorCursorShowDebug"
-    $ it "produces valid cursors"
-    $ producesValidsOnValids editorCursorShowDebug
-  describe "editorCursorHideDebug"
-    $ it "produces valid cursors"
-    $ producesValidsOnValids editorCursorHideDebug
-  describe "editorCursorToggleDebug"
-    $ it "produces valid cursors"
-    $ producesValidsOnValids editorCursorToggleDebug
+spec =
+  modifyMaxShrinks (const 1) $ do
+    describe "startEditorCursor" $ it "works on any valid smos file" $ forAllValid $ \sf ->
+      forAllValid $ \rp ->
+        withSystemTempDir "smos-test" $ \tdir -> do
+          let p = tdir </> rp
+          writeSmosFile p sf
+          errOrCursor <- startEditorCursor p
+          case errOrCursor of
+            Nothing -> expectationFailure "Locking should have been possible"
+            Just (Left err) -> expectationFailure err
+            Just (Right _) -> pure ()
