25bca8ab8e706e315bdaba7d45f014e6cac6b478
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-08-06 07:51:47 +0200

    first table tests

--- smos-calendar-import/test/Smos/Calendar/Import/RecurrenceRule/TableSpec.hs
+++ smos-calendar-import/test/Smos/Calendar/Import/RecurrenceRule/TableSpec.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
-
 module Smos.Calendar.Import.RecurrenceRule.TableSpec
   ( spec,
   )
@@ -18,13 +15,54 @@ import Test.Validity
 
 spec :: Spec
 spec =
+  -- TODO add 'cover' clauses to make sure these tests have value
   describe "dailyNextRecurrence" $ do
     specify "ByMonth limits the recurrence set" $ forAllValid $ \cur ->
-      forAll genDailyRecurrence $ \rrule ->
-        forAllValid $ \limit -> do
-          let withoutRule = rrule {rRuleByMonth = []}
-          let with = rruleOccurrencesUntil cur rrule limit
-          print (S.size with)
-          let without = rruleOccurrencesUntil cur rrule limit
-          print (S.size without)
-          S.size with `shouldSatisfy` (<= (S.size without))
+      forAll genDailyRecurrence $ \rrule -> do
+        let limit = addLocalTime (30 * nominalDay) cur
+        let withoutRule = rrule {rRuleByMonth = []}
+        let with = rruleOccurrencesUntil cur rrule limit
+        let without = rruleOccurrencesUntil cur rrule limit
+        S.size with `shouldSatisfy` (<= S.size without)
+    specify "ByMonthDay limits the recurrence set" $ forAllValid $ \cur ->
+      forAll genDailyRecurrence $ \rrule -> do
+        let limit = addLocalTime (30 * nominalDay) cur
+        let withoutRule = rrule {rRuleByMonthDay = []}
+        let with = rruleOccurrencesUntil cur rrule limit
+        let without = rruleOccurrencesUntil cur rrule limit
+        S.size with `shouldSatisfy` (<= S.size without)
+    specify "ByDay limits the recurrence set" $ forAllValid $ \cur ->
+      forAll genDailyRecurrence $ \rrule -> do
+        let limit = addLocalTime (30 * nominalDay) cur
+        let withoutRule = rrule {rRuleByDay = []}
+        let with = rruleOccurrencesUntil cur rrule limit
+        let without = rruleOccurrencesUntil cur rrule limit
+        S.size with `shouldSatisfy` (<= S.size without)
+    specify "ByHour expands the recurrence set" $ forAllValid $ \cur ->
+      forAll genDailyRecurrence $ \rrule -> do
+        let limit = addLocalTime (30 * nominalDay) cur
+        let withoutRule = rrule {rRuleByHour = []}
+        let with = rruleOccurrencesUntil cur rrule limit
+        let without = rruleOccurrencesUntil cur rrule limit
+        S.size with `shouldSatisfy` (>= S.size without)
+    specify "ByMinute expands the recurrence set" $ forAllValid $ \cur ->
+      forAll genDailyRecurrence $ \rrule -> do
+        let limit = addLocalTime (30 * nominalDay) cur
+        let withoutRule = rrule {rRuleByMinute = []}
+        let with = rruleOccurrencesUntil cur rrule limit
+        let without = rruleOccurrencesUntil cur rrule limit
+        S.size with `shouldSatisfy` (>= S.size without)
+    specify "BySecond expands the recurrence set" $ forAllValid $ \cur ->
+      forAll genDailyRecurrence $ \rrule -> do
+        let limit = addLocalTime (30 * nominalDay) cur
+        let withoutRule = rrule {rRuleBySecond = []}
+        let with = rruleOccurrencesUntil cur rrule limit
+        let without = rruleOccurrencesUntil cur rrule limit
+        S.size with `shouldSatisfy` (>= S.size without)
+    specify "BySetPos limits the recurrence set" $ forAllValid $ \cur ->
+      forAll genDailyRecurrence $ \rrule -> do
+        let limit = addLocalTime (30 * nominalDay) cur
+        let withoutRule = rrule {rRuleBySetPos = []}
+        let with = rruleOccurrencesUntil cur rrule limit
+        let without = rruleOccurrencesUntil cur rrule limit
+        S.size with `shouldSatisfy` (<= S.size without)
