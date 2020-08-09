cb4b4e1726bf1a975231599d28a737d0271003bc
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-08-09 07:46:31 +0200

    refactored out the weekdate code into a module

--- smos-calendar-import/src/Smos/Calendar/Import/RecurrenceRule.hs
+++ smos-calendar-import/src/Smos/Calendar/Import/RecurrenceRule.hs
@@ -9,7 +9,6 @@
 -- This module uses list as a monad a lot, make sure you understand it before reading this module.
 module Smos.Calendar.Import.RecurrenceRule where
 
-import Control.Exception
 import Control.Monad
 import Data.Aeson.Types (Pair)
 import Data.List
@@ -25,6 +24,7 @@ import Data.Validity.Time ()
 import Data.Yaml
 import GHC.Generics (Generic)
 import Safe
+import Smos.Calendar.Import.WeekDate
 import Smos.Data
 import YamlParse.Applicative
 
@@ -1043,10 +1043,10 @@ weeklyDayRecurrence
   byMonths
   weekStart
   byDays = do
-    let (y, WeekNo w, dow) = toWeekDateWithStart weekStart d_
+    let (y, w, dow) = toWeekDateWithStart weekStart d_
     d' <- takeWhile (<= limitDay) $ do
       i <- [0 ..]
-      maybeToList $ fromWeekDateWithStart weekStart y (WeekNo $ w + i * fromIntegral interval) dow
+      maybeToList $ fromWeekDateWithStart weekStart y (w + i * fromIntegral interval) dow
     d <-
       sort $ -- Need to sort because the week days may not be in order.
         if S.null byDays
@@ -1089,94 +1089,6 @@ byEveryWeekDayExpand weekStart byDays d =
       let (y, wn, _) = toWeekDateWithStart weekStart d
       dow <- S.toList byDays
       maybeToList $ fromWeekDateWithStart weekStart y wn dow
-
--- | Calculate the year, week number and weekday of a day, given a day on which the week starts
---
--- The BYWEEKNO rule part specifies a COMMA-separated list of
--- ordinals specifying weeks of the year.  Valid values are 1 to 53
--- or -53 to -1.  This corresponds to weeks according to week
--- numbering as defined in [ISO.8601.2004].  A week is defined as a
--- seven day period, starting on the day of the week defined to be
--- the week start (see WKST).  Week number one of the calendar year
--- is the first week that contains at least four (4) days in that
--- calendar year.
---
---    Note: Assuming a Monday week start, week 53 can only occur when
---    Thursday is January 1 or if it is a leap year and Wednesday is
---    January 1.
---
--- This means that in 2015, when Jan 1st was a thursday:
--- - with a week start of Monday, the first week started on 29 dec 2014
--- - with a week start of Sunday, the first week started on 4 jan 2015
-toWeekDateWithStart :: DayOfWeek -> Day -> (Integer, ByWeekNo, DayOfWeek)
-toWeekDateWithStart ws d =
-  let dow = dayOfWeek d
-      (year, _, _) = toGregorian d
-      firstDayOfTheFirstWsWeekThisYear = firstDayOfTheFirstWsWeekOf ws year
-      firstDayOfTheFirstWsWeekNextYear = firstDayOfTheFirstWsWeekOf ws (year + 1)
-      (wsWeekYear, wsWeekNo)
-        | d < firstDayOfTheFirstWsWeekThisYear = (year - 1, WeekNo 53) -- TODO leap year to see if it's 53 or 52
-        | d >= firstDayOfTheFirstWsWeekNextYear = (year + 1, WeekNo 1)
-        | otherwise = (year, WeekNo $ fromInteger $ (diffDays d firstDayOfTheFirstWsWeekThisYear `quot` 7) + 1)
-   in (wsWeekYear, wsWeekNo, dow)
-
-daysInYear :: Integer -> Int
-daysInYear y = if isLeapYear y then 366 else 365
-
-fromWeekDateWithStart :: DayOfWeek -> Integer -> ByWeekNo -> DayOfWeek -> Maybe Day
-fromWeekDateWithStart ws year (WeekNo w) dow =
-  Just $
-    let firstD = firstDayOfTheFirstWsWeekOf ws year
-        weekOffset = positiveMod 7 $ fromEnum dow - fromEnum ws
-     in addDays (fromIntegral $ 7 * (w -1) + weekOffset) firstD
-
--- We want to know whethere the first 'ws' occurs in the first week of
--- this year or in the last week of last year
--- Example: If the first 'ws' occurs on jan 1st then it's easy because
--- then it's definitely the first week of this year beacuse then all 7
--- days of that week are in this year.
--- To make sure that four of the days in the week that started on the
--- 'ws' week that contains jan 1, the 'ws' of that week must have
--- occurred on or after the third-to-last day of the previous year.
--- In that case the first week started in the previous year.
--- If that 'ws' occurred before the third-to-last day of the previous year,
--- then the first week started in the current year.
--- The third-to-last day of the previous year is always 29 dec, even in
--- leap years
---
--- For example, if Jan 1st is a thursday then the first monday-week of the year started this year
--- but if Jan 1st is a Wednesday then the first monday-week of the year started last year
---
--- If the 'firstDayOfTheWSWeekThatContainsJan1st' is on or after dec 29
--- then it is the first day of the first ws week otherwise, the first
--- week starts a week later.
-firstDayOfTheFirstWsWeekOf :: DayOfWeek -> Integer -> Day
-firstDayOfTheFirstWsWeekOf ws year =
-  let firstDayOfTheWSWeekThatContainsJan1stForD = firstDayOfTheWSWeekThatContainsJan1st ws year
-   in assert (dayOfWeek firstDayOfTheWSWeekThatContainsJan1stForD == ws) $
-        if firstDayOfTheWSWeekThatContainsJan1stForD >= fromGregorian (year - 1) 12 29
-          then firstDayOfTheWSWeekThatContainsJan1stForD
-          else addDays 7 firstDayOfTheWSWeekThatContainsJan1stForD
-
--- | The first 'ws' day of the week that contains jan 1st
---
--- Example 1: If Jan 1st is a thursday and the week starts on monday
--- then dec 29 is the first day of the 'monday'-week that contains jan 1st
--- so we have to subtract 3, which is positiveMod 7 (Thursday (4) - Monday (1))
---
--- Example 2: If Jan 1st is a thursday and the week starts on saturday
--- then then dec 27 is the firstay day of the 'monday'-week start contains jan 1st
--- so we have to subtract 5, which is positiveMod 7 (Thursday (4) - Saturday (6))
-firstDayOfTheWSWeekThatContainsJan1st :: DayOfWeek -> Integer -> Day
-firstDayOfTheWSWeekThatContainsJan1st ws year =
-  let firstDayOfTheYear = fromGregorian year 1 1
-      dowFirstDayOfTheYear = dayOfWeek firstDayOfTheYear
-   in addDays (negate $ positiveMod 7 $ fromIntegral $ fromEnum dowFirstDayOfTheYear - fromEnum ws) firstDayOfTheYear
-
-positiveMod :: Integral i => i -> i -> i
-positiveMod r n =
-  let m = n `mod` r
-   in if m < 0 then m + r else m
 
 monthIndices :: Day -> (Int, Int) -- (Positive index, Negative index)
 monthIndices d =
---
+++ smos-calendar-import/test/Smos/Calendar/Import/WeekDateSpec.hs
@@ -0,0 +1,108 @@
+{-# LANGUAGE OverloadedLists #-}
+
+module Smos.Calendar.Import.WeekDateSpec
+  ( spec,
+  )
+where
+
+import Data.Time
+import Smos.Calendar.Import.RecurrenceRule.Gen ()
+import Smos.Calendar.Import.WeekDate
+import Test.Hspec
+import Test.Validity
+
+spec :: Spec
+spec = do
+  let d = fromGregorian
+  describe "firstDayOfTheWSWeekThatContainsJan1st" $ do
+    it "works for a day in 2014 with a Monday week start" $
+      firstDayOfTheWSWeekThatContainsJan1st Monday 2014 `shouldBe` d 2013 12 30
+    it "works for a day in 2015 with a Monday week start" $
+      firstDayOfTheWSWeekThatContainsJan1st Monday 2015 `shouldBe` d 2014 12 29
+    it "works for a day in 2014 with a Sunday week start" $
+      firstDayOfTheWSWeekThatContainsJan1st Sunday 2014 `shouldBe` d 2013 12 29
+    it "works for a day in 2015 with a Sunday week start" $
+      firstDayOfTheWSWeekThatContainsJan1st Sunday 2015 `shouldBe` d 2014 12 28
+    it "works for a day in 2014 with a Thursday week start" $
+      firstDayOfTheWSWeekThatContainsJan1st Thursday 2014 `shouldBe` d 2013 12 26
+    it "works for a day in 2015 with a Thursday week start" $
+      firstDayOfTheWSWeekThatContainsJan1st Thursday 2015 `shouldBe` d 2015 01 01
+  describe "toWeekDateWithStart" $ do
+    describe "examples" $ do
+      it "works for 2013-12-29 in 2013 with a Monday weekstart" $
+        toWeekDateWithStart Monday (d 2013 12 29) `shouldBe` (2013, 52, Sunday)
+      it "works for 2014-01-01 in 2014 with a Monday weekstart" $
+        toWeekDateWithStart Monday (d 2014 01 01) `shouldBe` (2014, 01, Wednesday)
+      it "works for 2014-01-05 in 2014 with a Monday weekstart" $
+        toWeekDateWithStart Monday (d 2014 01 05) `shouldBe` (2014, 01, Sunday)
+      it "works for 2013-12-29 in 2013 with a Sunday weekstart" $
+        toWeekDateWithStart Sunday (d 2013 12 29) `shouldBe` (2014, 01, Sunday)
+      it "works for 2014-01-01 in 2014 with a Sunday weekstart" $
+        toWeekDateWithStart Sunday (d 2014 01 01) `shouldBe` (2014, 01, Wednesday)
+      it "works for 2014-01-05 in 2014 with a Sunday weekstart" $
+        toWeekDateWithStart Sunday (d 2014 01 05) `shouldBe` (2014, 02, Sunday)
+      it "works for 2014-12-29 in 2015 with a Monday weekstart" $
+        toWeekDateWithStart Monday (d 2014 12 29) `shouldBe` (2015, 01, Monday)
+      it "works for 2015-01-01 in 2015 with a Monday weekstart" $
+        toWeekDateWithStart Monday (d 2015 01 01) `shouldBe` (2015, 01, Thursday)
+      it "works for 2015-01-05 in 2015 with a Monday weekstart" $
+        toWeekDateWithStart Monday (d 2015 01 05) `shouldBe` (2015, 02, Monday)
+      it "works for 2014-12-29 in 2014 with a Sunday weekstart" $
+        toWeekDateWithStart Sunday (d 2014 12 29) `shouldBe` (2014, 53, Monday)
+      it "works for 2015-01-01 in 2014 with a Sunday weekstart" $
+        toWeekDateWithStart Sunday (d 2015 01 01) `shouldBe` (2014, 53, Thursday)
+      it "works for 2015-01-05 in 2015 with a Sunday weekstart" $
+        toWeekDateWithStart Sunday (d 2015 01 05) `shouldBe` (2015, 01, Monday)
+      it "works for 1997-01-01 in 1997 with a Monday weekstart" $
+        toWeekDateWithStart Monday (d 1997 01 01) `shouldBe` (1997, 01, Wednesday)
+      it "works for 1997-01-07 in 1997 with a Monday weekstart" $
+        toWeekDateWithStart Monday (d 1997 01 07) `shouldBe` (1997, 02, Tuesday)
+      it "works for 1997-01-01 in 1997 with a Sunday weekstart" $
+        toWeekDateWithStart Sunday (d 1997 01 01) `shouldBe` (1997, 01, Wednesday)
+      it "works for 1997-01-07 in 1997 with a Sunday weekstart" $
+        toWeekDateWithStart Sunday (d 1997 01 07) `shouldBe` (1997, 02, Tuesday)
+      it "works for 1997-08-17 in 1997 with a Sunday weekstart" $
+        toWeekDateWithStart Sunday (d 1997 08 17) `shouldBe` (1997, 34, Sunday)
+      it "works for 1997-08-17 in 1997 with a Sunday weekstart" $
+        toWeekDateWithStart Monday (d 1997 08 17) `shouldBe` (1997, 33, Sunday)
+    it "produces tuples of which the last value is independent of the week start" $ forAllValid $ \ws1 ->
+      forAllValid $ \ws2 ->
+        forAllValid $ \day ->
+          let (_, _, dow1) = toWeekDateWithStart ws1 day
+              (_, _, dow2) = toWeekDateWithStart ws2 day
+           in dow1 `shouldBe` dow2
+    it "produces valid results" $ producesValidsOnValids2 toWeekDateWithStart
+  describe "fromWeekDateWithStart" $ do
+    describe "examples" $ do
+      it "works for 2013-12-29 in 2013 with a Monday weekstart" $
+        fromWeekDateWithStart Monday 2013 52 Sunday `shouldBe` Just (d 2013 12 29)
+      it "works for 2014-01-01 in 2014 with a Monday weekstart" $
+        fromWeekDateWithStart Monday 2014 01 Wednesday `shouldBe` Just (d 2014 01 01)
+      it "works for 2014-01-05 in 2014 with a Monday weekstart" $
+        fromWeekDateWithStart Monday 2014 01 Sunday `shouldBe` Just (d 2014 01 05)
+      it "works for 2013-12-29 in 2013 with a Sunday weekstart" $
+        fromWeekDateWithStart Sunday 2014 01 Sunday `shouldBe` Just (d 2013 12 29)
+      it "works for 2014-01-01 in 2014 with a Sunday weekstart" $
+        fromWeekDateWithStart Sunday 2014 01 Wednesday `shouldBe` Just (d 2014 01 01)
+      it "works for 2014-01-05 in 2014 with a Sunday weekstart" $
+        fromWeekDateWithStart Sunday 2014 02 Sunday `shouldBe` Just (d 2014 01 05)
+      it "works for 2014-12-29 in 2015 with a Monday weekstart" $
+        fromWeekDateWithStart Monday 2015 01 Monday `shouldBe` Just (d 2014 12 29)
+      it "works for 2015-01-01 in 2015 with a Monday weekstart" $
+        fromWeekDateWithStart Monday 2015 01 Thursday `shouldBe` Just (d 2015 01 01)
+      it "works for 2015-01-05 in 2015 with a Monday weekstart" $
+        fromWeekDateWithStart Monday 2015 02 Monday `shouldBe` Just (d 2015 01 05)
+      it "works for 2014-12-29 in 2014 with a Sunday weekstart" $
+        fromWeekDateWithStart Sunday 2014 53 Monday `shouldBe` Just (d 2014 12 29)
+      it "works for 2015-01-01 in 2014 with a Sunday weekstart" $
+        fromWeekDateWithStart Sunday 2014 53 Thursday `shouldBe` Just (d 2015 01 01)
+      it "works for 2015-01-05 in 2015 with a Sunday weekstart" $
+        fromWeekDateWithStart Sunday 2015 01 Monday `shouldBe` Just (d 2015 01 05)
+    it "produces valid results" $ forAllValid $ producesValidsOnValids3 . fromWeekDateWithStart
+    xdescribe "This fails for very negative years for unknown reasons"
+      $ it "roundtrips toWeekDateWithStart for a given week start"
+      $ forAllValid
+      $ \ws ->
+        forAllValid $ \day ->
+          let (y, wn, dow) = toWeekDateWithStart ws day
+           in fromWeekDateWithStart ws y wn dow `shouldBe` Just day
