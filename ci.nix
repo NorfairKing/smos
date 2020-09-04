aeb6ff848dc68a2dcd752f28fff4a9963a3fbada
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-09-04 17:48:21 +0200

    Get the casts out of the ci build (explicitcly) so that they are not build on macos

--- ci.nix
+++ ci.nix
@@ -2,7 +2,7 @@ let
   pkgs = import ./nix/ci-pkgs.nix;
   pre-commit-hooks = import ./nix/pre-commit.nix;
 in
-pkgs.smosPackages // pkgs.smosCasts // {
+pkgs.smosPackages // {
   release = pkgs.smosRelease;
   pre-commit-check = pre-commit-hooks.run;
   inherit (pkgs) smosVersionInfo;
