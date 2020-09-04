57d9f675f20f60fbeb23760e26b22eeb18ba7cc5
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-09-04 04:43:57 +0200

    failing test

--- smos-sync-client-gen/test/Smos/Sync/Client/Sync/MetaSpec.hs
+++ smos-sync-client-gen/test/Smos/Sync/Client/Sync/MetaSpec.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Smos.Sync.Client.Sync.MetaSpec
   ( spec,
   )
 where
 
+import qualified Data.Mergeful as Mergeful
 import Data.Pool
 import Database.Persist.Sqlite as DB
+import Path
+import Smos.API.SHA256 as SHA256
+import Smos.Sync.Client.DB
 import Smos.Sync.Client.Meta
 import Smos.Sync.Client.MetaMap.Gen ()
 import Smos.Sync.Client.TestUtils
@@ -13,28 +20,38 @@ import Test.Validity
 
 spec :: Spec
 spec = do
-  describe "makeClientMetaData"
-    $ it "produces valid meta maps"
-    $ producesValidsOnValids2 makeClientMetaData
-  clientDBSpec
-    $ describe "writeClientMetadata"
-    $ do
-      it "can read exactly what was just written" $ \pool ->
-        forAllValid $ \m -> do
-          m' <-
-            testDB pool $ do
-              writeClientMetadata m
-              readClientMetadata
-          m' `shouldBe` m
-      it "can read exactly what was just written, even if something else has been written first" $ \pool ->
-        forAllValid $ \m1 ->
-          forAllValid $ \m2 -> do
-            m' <-
-              testDB pool $ do
-                writeClientMetadata m1
-                writeClientMetadata m2
-                readClientMetadata
-            m' `shouldBe` m2
+  describe "makeClientMetaData" $ it "produces valid meta maps" $ producesValidsOnValids2 makeClientMetaData
+  clientDBSpec $ describe "writeClientMetadata" $ do
+    it "can read exactly what was just written" $ \pool ->
+      forAllValid $ \m -> do
+        m' <- testDB pool $ do
+          writeClientMetadata m
+          readClientMetadata
+        m' `shouldBe` m
+    it "can read exactly what was just written, even if something else has been written first" $ \pool ->
+      forAllValid $ \m1 ->
+        forAllValid $ \m2 -> do
+          m' <- testDB pool $ do
+            writeClientMetadata m1
+            writeClientMetadata m2
+            readClientMetadata
+          m' `shouldBe` m2
+    it "does not crash while reading this nasty situation" $ \pool -> do
+      m <- testDB pool $ do
+        insertMany_
+          [ ClientFile
+              { clientFilePath = [relfile|foo|],
+                clientFileSha256 = SHA256.hashBytes "abc",
+                clientFileTime = Mergeful.initialServerTime
+              },
+            ClientFile
+              { clientFilePath = [relfile|foo/bar|],
+                clientFileSha256 = SHA256.hashBytes "cde",
+                clientFileTime = Mergeful.initialServerTime
+              }
+          ]
+        readClientMetadata
+      shouldBeValid m
 
 testDB :: Pool SqlBackend -> SqlPersistT IO a -> IO a
 testDB = flip DB.runSqlPool
