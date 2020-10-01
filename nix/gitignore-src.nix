c3698b427f5f3e033aabb27d8e68a2f25247262d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-10-01 10:38:46 +0200

    fetch gitignore.nix using fetchGit instead of fetchFromGitHub

--- nix/gitignore-src.nix
+++ nix/gitignore-src.nix
@@ -1,11 +1,10 @@
 final: previous:
 let
   gitignoreSrc =
-    final.fetchFromGitHub {
-      owner = "hercules-ci";
-      repo = "gitignore";
-      rev = "ec4a0039152655b6c919d289dafd7ba32206ea1f";
-      sha256 = "sha256:13qxqbs8jg2mz2fm2cs63czv30gxi39ws5qzf9j8mczqpdj6g3im";
+    builtins.fetchGit {
+      url = "https://github.com/hercules-ci/gitignore.nix";
+      ref = "master";
+      rev = "c4662e662462e7bf3c2a968483478a665d00e717";
     };
 in
 {
