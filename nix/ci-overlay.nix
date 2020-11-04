fc8cfe9d5cfa0c2dda49bf821dbbede013fab55f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-11-04 11:38:46 +0100

    removed an obsolete file

--- nix/ci-overlay.nix
+++
@@ -1,9 +0,0 @@
-final: previous:
-let
-  ciVersionInfo = final.writeText "ci-version-info" ''
-    This is a CI build without version information. You should not be using this in practice because then you cannot submit good bug reports.
-  '';
-in
-{
-  smosVersionInfo = ciVersionInfo;
-}
