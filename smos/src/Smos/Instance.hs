6de8eecc8e00d76e4fcb600343b2dc814026d6de
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-11-23 23:42:55 +0100

    another refactor

--- smos/src/Smos/Instance.hs
+++ smos/src/Smos/Instance.hs
@@ -1,48 +1,47 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-
-module Smos.Instance where
+module Smos.Instance
+  ( module Smos.Instance,
+    module Smos.Terminal,
+  )
+where
 
 import Conduit
-import Data.Aeson
 import Data.ByteString (ByteString)
-import Data.Word
 import qualified Graphics.Vty as Vty (Config (..), defaultConfig, mkVty)
 import Graphics.Vty.Output.TerminfoBased (setWindowSize)
 import Smos
+import Smos.Terminal
 import Smos.Types
 import System.Posix
 import UnliftIO
 
-data SmosInstanceHandle
-  = SmosInstanceHandle
-      { smosInstanceHandleMasterHandle :: !Handle,
-        smosInstanceHandleSlaveHandle :: !Handle,
-        smosInstanceHandleResizeFd :: Fd,
-        smosInstanceHandleAsync :: Async ()
-      }
+type SmosInstanceHandle = TerminalHandle
 
 withSmosInstance :: MonadUnliftIO m => SmosConfig -> Maybe StartingPath -> (SmosInstanceHandle -> m a) -> m a
-withSmosInstance config mStartingPath func = do
-  (masterFd, slaveFd) <- liftIO openPseudoTerminal
-  let smosInstanceHandleResizeFd = slaveFd
-  smosInstanceHandleMasterHandle <- liftIO $ fdToHandle masterFd
-  flip finally (hClose smosInstanceHandleMasterHandle) $ do
-    smosInstanceHandleSlaveHandle <- liftIO $ fdToHandle slaveFd
-    flip finally (hClose smosInstanceHandleSlaveHandle) $ do
-      let vtyBuilder = do
-            vty <-
-              Vty.mkVty $
-                Vty.defaultConfig
-                  { Vty.inputFd = Just slaveFd,
-                    Vty.outputFd = Just slaveFd
-                  }
-            setWindowSize smosInstanceHandleResizeFd (80, 24)
-            pure vty
-      let runSmos :: MonadIO m => m ()
-          runSmos = liftIO $ startSmosWithVtyBuilderOn vtyBuilder mStartingPath config
-      withAsync runSmos $ \smosInstanceHandleAsync ->
-        func SmosInstanceHandle {..}
+withSmosInstance config mStartingPath = withTerminal $ \_ slaveFd ->
+  let vtyBuilder = do
+        vty <-
+          Vty.mkVty $
+            Vty.defaultConfig
+              { Vty.inputFd = Just slaveFd,
+                Vty.outputFd = Just slaveFd
+              }
+        setWindowSize slaveFd (80, 24)
+        pure vty
+      runSmos :: MonadIO m => m ()
+      runSmos = liftIO $ startSmosWithVtyBuilderOn vtyBuilder mStartingPath config
+   in runSmos
+
+smosInstanceHandleMasterHandle :: SmosInstanceHandle -> Handle
+smosInstanceHandleMasterHandle = terminalHandleMasterHandle
+
+smosInstanceHandleSlaveHandle :: SmosInstanceHandle -> Handle
+smosInstanceHandleSlaveHandle = terminalHandleSlaveHandle
+
+smosInstanceHandleResizeFd :: SmosInstanceHandle -> Fd
+smosInstanceHandleResizeFd = terminalHandleResizeFd
+
+smosInstanceHandleAsync :: SmosInstanceHandle -> Async ()
+smosInstanceHandleAsync = terminalHandleAsync
 
 smosInstanceInputSink :: MonadIO m => SmosInstanceHandle -> ConduitT ByteString o m ()
 smosInstanceInputSink = sinkHandle . smosInstanceHandleMasterHandle
@@ -50,20 +49,5 @@ smosInstanceInputSink = sinkHandle . smosInstanceHandleMasterHandle
 smosInstanceOutputSource :: MonadIO m => SmosInstanceHandle -> ConduitT i ByteString m ()
 smosInstanceOutputSource = sourceHandle . smosInstanceHandleMasterHandle
 
-data TerminalSize
-  = TerminalSize
-      { terminalWidth :: !Word16,
-        terminalHeight :: !Word16
-      }
-  deriving (Show)
-
-instance FromJSON TerminalSize where
-  parseJSON =
-    withObject "TerminalSize" $ \o -> do
-      terminalWidth <- o .: "width"
-      terminalHeight <- o .: "height"
-      pure TerminalSize {..}
-
 smosInstanceResize :: SmosInstanceHandle -> TerminalSize -> IO ()
-smosInstanceResize SmosInstanceHandle {..} TerminalSize {..} =
-  setWindowSize smosInstanceHandleResizeFd (fromIntegral terminalWidth, fromIntegral terminalHeight)
+smosInstanceResize = terminalResize
---
+++ smos/src/Smos/Terminal.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Smos.Terminal where
+
+import Conduit
+import Data.Aeson
+import Data.ByteString (ByteString)
+import Data.Word
+import qualified Graphics.Vty as Vty (Config (..), defaultConfig, mkVty)
+import Graphics.Vty.Output.TerminfoBased (setWindowSize)
+import Smos
+import Smos.Types
+import System.Posix
+import UnliftIO
+
+data TerminalHandle
+  = TerminalHandle
+      { terminalHandleMasterHandle :: !Handle,
+        terminalHandleSlaveHandle :: !Handle,
+        terminalHandleResizeFd :: Fd,
+        terminalHandleAsync :: Async ()
+      }
+
+withTerminal :: MonadUnliftIO m => (Handle -> Fd -> m ()) -> (TerminalHandle -> m a) -> m a
+withTerminal programFunc func = do
+  (masterFd, slaveFd) <- liftIO openPseudoTerminal
+  let terminalHandleResizeFd = slaveFd
+  terminalHandleMasterHandle <- liftIO $ fdToHandle masterFd
+  flip finally (hClose terminalHandleMasterHandle) $ do
+    terminalHandleSlaveHandle <- liftIO $ fdToHandle slaveFd
+    flip finally (hClose terminalHandleSlaveHandle) $ do
+      let runProgram = programFunc terminalHandleSlaveHandle terminalHandleResizeFd
+      withAsync runProgram $ \terminalHandleAsync -> do
+        liftIO $ setWindowSize terminalHandleResizeFd (80, 24)
+        let th = TerminalHandle {..}
+        func th
+
+terminalInputSink :: MonadIO m => TerminalHandle -> ConduitT ByteString o m ()
+terminalInputSink = sinkHandle . terminalHandleMasterHandle
+
+terminalOutputSource :: MonadIO m => TerminalHandle -> ConduitT i ByteString m ()
+terminalOutputSource = sourceHandle . terminalHandleMasterHandle
+
+data TerminalSize
+  = TerminalSize
+      { terminalWidth :: !Word16,
+        terminalHeight :: !Word16
+      }
+  deriving (Show)
+
+instance FromJSON TerminalSize where
+  parseJSON =
+    withObject "TerminalSize" $ \o -> do
+      terminalWidth <- o .: "width"
+      terminalHeight <- o .: "height"
+      pure TerminalSize {..}
+
+terminalResize :: TerminalHandle -> TerminalSize -> IO ()
+terminalResize TerminalHandle {..} TerminalSize {..} =
+  setWindowSize terminalHandleResizeFd (fromIntegral terminalWidth, fromIntegral terminalHeight)
