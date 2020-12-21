8a313443ed75521660bf42e30358b0b824fff1b3
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-12-21 13:37:59 +0100

    fixed flakiness of smos-query tests

--- smos-query/test/Smos/Query/WaitingSpec.hs
+++ smos-query/test/Smos/Query/WaitingSpec.hs
@@ -5,8 +5,9 @@ import Test.Syd
 import Test.Syd.Validity
 
 spec :: Spec
-spec = modifyMaxSuccess (`div` 50) $ -- The first test will be empty, the second will not
-  describe "Waiting" $
-    it "'just works' for any InterestingStore" $
-      forAllValid $
-        \is -> testSmosQuery is ["waiting"]
+spec = sequential $
+  modifyMaxSuccess (`div` 50) $ -- The first test will be empty, the second will not
+    describe "Waiting" $
+      it "'just works' for any InterestingStore" $
+        forAllValid $
+          \is -> testSmosQuery is ["waiting"]
--- smos-query/test/Smos/Query/WorkSpec.hs
+++ smos-query/test/Smos/Query/WorkSpec.hs
@@ -5,8 +5,9 @@ import Test.Syd
 import Test.Syd.Validity
 
 spec :: Spec
-spec = modifyMaxSuccess (`div` 50) $ -- The first test will be empty, the second will not
-  describe "Work" $
-    it "'just works' for any InterestingStore" $
-      forAllValid $
-        \is -> testSmosQuery is ["work"]
+spec = sequential $
+  modifyMaxSuccess (`div` 50) $ -- The first test will be empty, the second will not
+    describe "Work" $
+      it "'just works' for any InterestingStore" $
+        forAllValid $
+          \is -> testSmosQuery is ["work"]
