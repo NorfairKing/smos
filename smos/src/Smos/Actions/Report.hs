72e6bcff38ae499ad670a82e161f9d8a846afee7
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-01-02 08:37:26 -0500

    move exitReport to Smos.Actions.Report

--- smos/src/Smos/Actions/Report.hs
+++ smos/src/Smos/Actions/Report.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Smos.Actions.Report where
 
-import Smos.Actions.Report.Exit
+import Smos.Actions.Browser
+import Smos.Actions.File
 import Smos.Actions.Report.Next
 import Smos.Actions.Report.Timestamps
 import Smos.Actions.Report.Waiting
@@ -9,17 +12,38 @@ import Smos.Types
 allPlainReportActions :: [Action]
 allPlainReportActions =
   concat
-    [ allPlainReportExitActions, 
+    [ allPlainReportExitActions,
       allPlainReportNextActions,
       allPlainReportWaitingActions,
       allPlainReportTimestampsActions
     ]
 
+
+allPlainReportExitActions :: [Action]
+allPlainReportExitActions =
+  [ exitReport
+  ]
+
 allReportUsingActions :: [ActionUsing Char]
 allReportUsingActions =
   concat
-    [ allReportExitUsingActions,
-      allReportNextActionsUsingActions,
+    [ allReportNextActionsUsingActions,
       allReportWaitingUsingActions,
       allReportTimestampsUsingActions
     ]
+
+
+-- Exit a Report
+-- If there is a file open, go to it (this already works via another Action?)
+-- If there is no file open, go to the browser in the workflow dir
+exitReport :: Action
+exitReport =
+  Action
+    { actionName = "exitReport",
+      actionDescription = "Exit any smos report, back to open file or browser",
+      actionFunc = do
+        ec <- gets smosStateCursor
+        case editorCursorLastOpenedFile ec of
+          Just fp -> switchToFile fp
+          Nothing -> actionFunc selectBrowserWorkflow
+    }
