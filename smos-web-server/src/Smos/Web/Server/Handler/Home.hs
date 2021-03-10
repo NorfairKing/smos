57c0c7a4e3e8dfc13530fdce634ad02e86147403
Hendrik Steurs <hendrik.steurs@gmail.com>
2021-03-10 14:01:53 +0100

    landing page smos updates

--- smos-web-server/src/Smos/Web/Server/Handler/Home.hs
+++ smos-web-server/src/Smos/Web/Server/Handler/Home.hs
@@ -7,9 +7,9 @@ import Smos.Web.Server.Static
 import Smos.Web.Server.Foundation
 import Smos.Web.Server.Widget
 import Yesod hiding (Header)
-import Yesod.Auth
 
 getHomeR :: Handler Html
-getHomeR = do
-  maid <- maybeAuthId
-  withNavBar $(widgetFile "home")
+getHomeR =
+  withNavBar $ do
+    addScriptRemote "https://cdn.jsdelivr.net/npm/bulma-carousel@4.0.3/dist/js/bulma-carousel.min.js"
+    $(widgetFile "home")
