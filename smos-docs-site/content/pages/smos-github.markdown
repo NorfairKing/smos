ae9557320365ac2dfdbee25c437e033565320409
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-22 04:18:31 +0100

    documentation for the new smos-github command

---
+++ smos-docs-site/content/pages/smos-github.markdown
@@ -0,0 +1,7 @@
+---
+title: The Smos GitHub Tool
+description: Documentation for the Smos GitHub Tool, for integration with GitHub.
+---
+
+To use this tool, you will want to set the `SMOS_GITHUB_OAUTH_TOKEN` environment variable (see the `Environment` reference below).
+You can follow [this guide from GitHub](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token) to create one.
---
+++ smos-docs-site/src/Smos/Docs/Site/Handler/SmosGitHub.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Smos.Docs.Site.Handler.SmosGitHub
+  ( getSmosGitHubR,
+  )
+where
+
+import qualified Env
+import Options.Applicative
+import Options.Applicative.Help
+import Smos.Docs.Site.Handler.Import
+import Smos.GitHub.OptParse as GitHub
+import YamlParse.Applicative
+
+getSmosGitHubR :: Handler Html
+getSmosGitHubR = do
+  DocPage {..} <- lookupPage "smos-github"
+  let argsHelpText = getHelpPageOf []
+      envHelpText = Env.helpDoc GitHub.prefixedEnvironmentParser
+      confHelpText = prettySchemaDoc @GitHub.Configuration
+  defaultLayout $ do
+    setSmosTitle "smos-github"
+    setDescription "Documentation for the Smos GitHub tool"
+    $(widgetFile "args")
+
+getHelpPageOf :: [String] -> String
+getHelpPageOf args =
+  let res = runArgumentsParser $ args ++ ["--help"]
+   in case res of
+        Failure fr ->
+          let (ph, _, cols) = execFailure fr "smos-github"
+           in renderHelp cols ph
+        _ -> error "Something went wrong while calling the option parser."
