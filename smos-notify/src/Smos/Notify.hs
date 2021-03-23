739303108083008344fa24632692119500c4ddcf
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-03-23 07:48:47 +0100

    example of sending a notification

--- smos-notify/src/Smos/Notify.hs
+++ smos-notify/src/Smos/Notify.hs
@@ -1,8 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Smos.Notify where
 
+import Conduit
+import Data.Text (Text)
+import qualified Data.Text as T
+import Path
+import Path.IO
 import Smos.Notify.OptParse
+import Smos.Report.Archive
+import Smos.Report.Config
+import Smos.Report.ShouldPrint
+import Smos.Report.Streaming
+import System.Exit
+import System.IO
+import System.Process
 
 smosNotify :: IO ()
 smosNotify = do
-  sets <- getSettings
-  print sets
+  -- sets@Settings {..} <- getSettings
+  -- print sets
+  -- wd <- resolveDirWorkflowDir setDirectorySettings
+  -- fs <- runConduit $ streamSmosFilesFromWorkflowRel HideArchive setDirectorySettings .| parseSmosFilesRel wd .| printShouldPrint (PrintWarning stderr) .| smosFileEntries .| sinkList
+  -- mapM_ print fs
+  executable <- findNotifySend
+  let example = Notification {notificationSummary = "hello world", notificationBody = "foo bar\nquux"}
+  displayNotification executable example
+
+data Notification = Notification
+  { notificationSummary :: Text,
+    notificationBody :: Text
+  }
+  deriving (Show, Eq)
+
+findNotifySend :: IO (Path Abs File)
+findNotifySend = do
+  rp <- parseRelFile "notify-send"
+  me <- findExecutable rp
+  case me of
+    Nothing -> die "could not find a notify-send executable."
+    Just e -> pure e
+
+displayNotification :: Path Abs File -> Notification -> IO ()
+displayNotification e Notification {..} = do
+  callProcess
+    (fromAbsFile e)
+    [ "--urgency=critical",
+      "--app-name=smos",
+      "--icon=/home/syd/src/cs-syd.eu/site/logo/positive/logo.png",
+      T.unpack notificationSummary,
+      T.unpack notificationBody
+    ]
