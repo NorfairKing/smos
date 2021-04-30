af1f8ed1ffb7543eee89cf43cc4249086801e34f
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-30 19:49:23 +0000

    get it right (with Syd's help of course ;)

--- smos-report-gen/test/Smos/Report/TimeSpec.hs
+++ smos-report-gen/test/Smos/Report/TimeSpec.hs
@@ -21,7 +21,30 @@ spec = do
   ordSpecOnValid @Time
   genValidSpec @Time
   jsonSpecOnValid @Time
-  describe "timeP" $ parsesValidSpec timeP
+  describe "timeP" $ do
+    parsesValidSpec timeP
+    it "parses 2s correctly" $ parseJust timeP "2s" (Seconds 2)
+    it "parses 2 sec correctly" $ parseJust timeP "2 sec" (Seconds 2)
+    it "parses 2 secs correctly" $ parseJust timeP "2 secs" (Seconds 2)
+    it "parses 2 second correctly" $ parseJust timeP "2 second" (Seconds 2)
+    it "parses 2 seconds correctly" $ parseJust timeP "2 seconds" (Seconds 2)
+    it "parses 3w correctly" $ parseJust timeP "3w" (Weeks 3)
+    it "parses 3 wk correctly" $ parseJust timeP "3 wk" (Weeks 3)
+    it "parses 3 wks correctly" $ parseJust timeP "3 wks" (Weeks 3)
+    it "parses 3 week correctly" $ parseJust timeP "3 week" (Weeks 3)
+    it "parses 3 weeks correctly" $ parseJust timeP "3 weeks" (Weeks 3)
+    it "parses 4d correctly" $ parseJust timeP "4d" (Days 4)
+    it "parses 4 day correctly" $ parseJust timeP "4 day" (Days 4)
+    it "parses 4 days correctly" $ parseJust timeP "4 days" (Days 4)
+    it "parses 5h correctly" $ parseJust timeP "5h" (Hours 5)
+    it "parses 5 hr correctly" $ parseJust timeP "5 hr" (Hours 5)
+    it "parses 5 hrs correctly" $ parseJust timeP "5 hrs" (Hours 5)
+    it "parses 5 hours correctly" $ parseJust timeP "5 hours" (Hours 5)
+    it "parses 6m correctly" $ parseJust timeP "6m" (Minutes 6)
+    it "parses 6 min correctly" $ parseJust timeP "6 min" (Minutes 6)
+    it "parses 6 mins correctly" $ parseJust timeP "6 mins" (Minutes 6)
+    it "parses 6 minute correctly" $ parseJust timeP "6 minute" (Minutes 6)
+    it "parses 6 minutes correctly" $ parseJust timeP "6 minutes" (Minutes 6)
   describe "renderTime" $ do
     it "produces valid texts" $ producesValidsOnValids renderTime
     it "renders bys that parse to the same" $ forAllValid $ \s -> parseJust timeP (renderTime s) s
