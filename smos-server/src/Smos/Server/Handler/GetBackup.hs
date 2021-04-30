2d9d7078e660c14ed3589e615722427926449227
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-04-30 13:47:20 +0200

    use a conduit for the backup download, for better memory usage

--- smos-server/src/Smos/Server/Handler/GetBackup.hs
+++ smos-server/src/Smos/Server/Handler/GetBackup.hs
@@ -7,7 +7,9 @@ module Smos.Server.Handler.GetBackup
 where
 
 import Codec.Archive.Zip as Zip
+import Conduit
 import Data.ByteString (ByteString)
+import qualified Data.Conduit.Combinators as C
 import Servant.Types.SourceT as Source
 import Smos.Server.Handler.Import
 import System.FilePath.Posix as Posix
@@ -23,23 +25,26 @@ serveGetBackup AuthCookie {..} uuid = withUserId authCookieUsername $ \uid -> do
       systemTempDir <- getTempDir
       tmpDir <- createTempDir systemTempDir "smos-server-temp-archive-dir"
       tempArchiveFileName <- resolveFile tmpDir "backup.zip"
-      backupFiles <- runDB $ selectList [BackupFileBackup ==. bid] []
-      -- Create a zip archive
-      Zip.createArchive (fromAbsFile tempArchiveFileName) $
-        forM_ backupFiles $ \(Entity _ BackupFile {..}) -> do
-          -- NOTE: Running 'Zip.mkEntrySelector' in 'Maybe' instead of 'IO'
-          -- (because it can be run in any 'MonadThrow') means that we could be
-          -- missing files in the resulting zip file.  However: We think that
-          -- that is better than not being able to download any backups.
-          let mSelector :: Maybe Zip.EntrySelector
-              mSelector = Zip.mkEntrySelector (prepareBackupFilePath backupFilePath)
-          let contents = decompressByteStringOrErrorMessage backupFileContents
-          forM mSelector $ Zip.addEntry Zip.Deflate contents
-      -- Stream the zip archive
-      pure $
-        SourceT $ \step -> do
-          let SourceT func = Source.readFile (fromAbsFile tempArchiveFileName)
-          func step `finally` removeDirRecur tmpDir
+      runDB $ do
+        ackBackupFileSource <- selectSourceRes [BackupFileBackup ==. bid] []
+        withAcquire ackBackupFileSource $ \backupFileSource -> do
+          -- Create a zip archive
+          Zip.createArchive (fromAbsFile tempArchiveFileName) $ do
+            let insertBackupFile (Entity _ BackupFile {..}) = do
+                  -- NOTE: Running 'Zip.mkEntrySelector' in 'Maybe' instead of 'IO'
+                  -- (because it can be run in any 'MonadThrow') means that we could be
+                  -- missing files in the resulting zip file.  However: We think that
+                  -- that is better than not being able to download any backups.
+                  let mSelector :: Maybe Zip.EntrySelector
+                      mSelector = Zip.mkEntrySelector (prepareBackupFilePath backupFilePath)
+                  let contents = decompressByteStringOrErrorMessage backupFileContents
+                  forM_ mSelector $ Zip.addEntry Zip.Deflate contents
+            runConduit $ backupFileSource .| C.mapM_ insertBackupFile
+          -- Stream the zip archive
+          pure $
+            SourceT $ \step -> do
+              let SourceT func = Source.readFile (fromAbsFile tempArchiveFileName)
+              func step `finally` removeDirRecur tmpDir
 
 prepareBackupFilePath :: Path Rel File -> FilePath
 prepareBackupFilePath = Windows.makeValid . Posix.makeValid . fromRelFile
