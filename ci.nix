fd420066890ee9919f50573f680056850d7e4d02
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-05-05 08:25:34 +0200

    cleanup the nix build

--- ci.nix
+++ ci.nix
@@ -5,9 +5,7 @@ let
 in
 {
   "release" = pkgsNormal.smosRelease;
-  "casts" = pkgsNormal.smosCasts;
   "release-static" = pkgsStatic.smosRelease;
-  "casts-static" = pkgsStatic.smosCasts;
   "pre-commit-hooks" = pre-commit-hooks.run;
   "release-zip" = pkgsStatic.smosReleaseZip;
   "nixos-module-test" = import ./nix/nixos-module-test.nix { pkgs = pkgsNormal; };
--- static.nix
+++
@@ -1,4 +0,0 @@
-let
-  pkgs = import ./nix/pkgs.nix { static = true; };
-in
-pkgs.smosPackages
