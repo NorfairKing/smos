86025b405fb7fd33c15d1ea39549bcf71594992d
Niv updater <syd@cs-syd.eu>
2021-05-12 10:55:23 +0200

    Automatic niv-update 2021-05-12

--- scripts/niv-update-pr.sh
+++ scripts/niv-update-pr.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env nix-shell
 #! nix-shell ./niv-update-pr.nix -i bash --keep GITHUB_TOKEN
 
-niv update 
+niv update
+git submodule update --init --recursive
 
 if ! git diff-index --quiet HEAD
 then
@@ -9,6 +10,7 @@ then
   branchname="package-update-$today"
   git checkout -b "$branchname"
   git config user.name "Niv updater"
+  pre-commit run
   git add .
   git commit -m "Automatic niv-update $today"
   git push -u origin "$branchname"
