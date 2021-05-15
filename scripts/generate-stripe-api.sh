4f5e1b776d079a67979f9e23a4690e8f223934b3
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-05-15 18:43:50 +0200

    generating a stripe client

---
+++ scripts/generate-stripe-api.sh
@@ -0,0 +1,23 @@
+export PATH="$HOME/.local/bin:$PATH"
+
+specRepo=${1:-stripe-api/stripe-spec}
+outputDir=${2:-stripe-api/stripe-client}
+openapi3-code-generator-exe "$specRepo/openapi/spec3.yaml" \
+  --property-type-suffix="'" \
+  --module-name "StripeClient" \
+  --convert-to-camel-case \
+  --package-name "stripe-client" \
+  --output-dir "$outputDir" \
+  --force \
+  \
+  --omit-additional-operation-functions \
+  --operation-to-generate "GetEvents" \
+  --operation-to-generate "GetCustomers" \
+  --operation-to-generate "PostCheckoutSessions" \
+  --white-listed-schema event \
+  --white-listed-schema notification_event_data \
+  --white-listed-schema checkout.session \
+  --white-listed-schema customer
+
+tree $outputDir/src/StripeClient/
+
