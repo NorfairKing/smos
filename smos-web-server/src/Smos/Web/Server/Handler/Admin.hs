447b5b7dfd081f307fe6717c1ec65da3f2bcd474
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-05-20 23:50:32 +0200

    allow admins to override the subscription of users

--- smos-web-server/src/Smos/Web/Server/Handler/Admin.hs
+++ smos-web-server/src/Smos/Web/Server/Handler/Admin.hs
@@ -2,7 +2,12 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module Smos.Web.Server.Handler.Admin where
+module Smos.Web.Server.Handler.Admin
+  ( getAdminPanelR,
+    getAdminUserR,
+    postAdminUserSetSubscriptionR,
+  )
+where
 
 import Smos.Web.Server.Handler.Import
 
@@ -16,4 +21,11 @@ getAdminUserR :: Username -> Handler Html
 getAdminUserR username = withAdminLogin $ \t -> do
   user <- runClientOrErr $ clientGetUser t username
   now <- liftIO getCurrentTime
+  token <- genToken
   withNavBar $(widgetFile "admin/user")
+
+postAdminUserSetSubscriptionR :: Username -> Handler Html
+postAdminUserSetSubscriptionR username = withAdminLogin $ \t -> do
+  endDate <- runInputPost $ ireq dayField "end-date"
+  NoContent <- runClientOrErr $ clientPutUserSubscription t username $ UTCTime endDate 0
+  redirect $ AdminUserR username
