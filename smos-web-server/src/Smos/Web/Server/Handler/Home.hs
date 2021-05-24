d24736414feb29c7a7381a4b328b9a9b5ff4cb7c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-05-24 09:21:37 +0200

    pricing page

--- smos-web-server/src/Smos/Web/Server/Handler/Home.hs
+++ smos-web-server/src/Smos/Web/Server/Handler/Home.hs
@@ -3,12 +3,21 @@
 
 module Smos.Web.Server.Handler.Home where
 
+import qualified Data.Text as T
 import Smos.Web.Server.Handler.Import
 
 getHomeR :: Handler Html
 getHomeR = do
   mDocsUrl <- getsYesod appDocsBaseUrl
   mMonetisation <- runClientOrErr clientGetMonetisation
+  let priceText m =
+        T.unwords
+          [ -- by 12 to get the price per month in cents, by 100 to get the price pr month in units
+            T.pack (printf "%d" (monetisationStripePricePerYear m `div` 12 `div` 100)),
+            T.toUpper (monetisationStripePriceCurrency m),
+            "/",
+            "Month"
+          ]
   withNavBar $ do
     addScript $ StaticR bulma_carousel_js
     addScript $ StaticR asciinema_player_js
