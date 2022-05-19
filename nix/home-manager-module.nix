1254ed41610e5b545f6fb61e579cd2e6906be26d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-05-19 16:21:27 +0200

    More descriptions for home manager module options

--- nix/home-manager-module.nix
+++ nix/home-manager-module.nix
@@ -15,7 +15,7 @@ let
 in
 {
   options.programs.smos = {
-    enable = mkEnableOption "Smos cli and syncing";
+    enable = mkEnableOption "Smos";
     smosReleasePackages = mkOption {
       description = "The smosPackages attribute defined in the nix/overlay.nix file in the smos repository.";
       default = (import ./pkgs.nix { }).smosReleasePackages;
@@ -25,146 +25,132 @@ in
       default = { };
     };
     workflowDir = mkOption {
-      type = types.str;
       description = "Smos' workflow directory";
+      type = types.str;
       default = config.home.homeDirectory + "/workflow";
     };
     backup = mkOption {
-      default = null;
-      type = types.nullOr (
-        types.submodule {
-          options = {
-            enable = mkEnableOption "Smos backups";
-            backupDir = mkOption {
-              type = types.str;
-              default = "${config.xdg.dataHome}/smos/backup";
-              description = "The directory to backup to";
-            };
+      description = "Periodic local backups of the workflow directory";
+      type = types.nullOr (types.submodule {
+        options = {
+          enable = mkEnableOption "Smos backups";
+          backupDir = mkOption {
+            type = types.str;
+            default = "${config.xdg.dataHome}/smos/backup";
+            description = "The directory to backup to";
           };
-        }
-      );
+        };
+      });
+      default = null;
     };
     sync = mkOption {
+      description = "Periodic local backups of the workflow directory";
+      type = types.nullOr (types.submodule {
+        options = {
+          enable = mkEnableOption "Smos syncing";
+          server-url = mkOption {
+            type = types.str;
+            example = "api.smos.cs-syd.eu";
+            description = "The url of the sync server";
+          };
+          username = mkOption {
+            type = types.str;
+            example = "syd";
+            description = "The username to use when logging into the sync server";
+          };
+          password = mkOption {
+            type = types.str;
+            example = "hunter12";
+            description = "The password to use when logging into the sync server";
+          };
+        };
+      });
       default = null;
-      type = types.nullOr (
-        types.submodule {
-          options =
-            {
-              enable = mkEnableOption "Smos syncing";
-              server-url =
-                mkOption {
-                  type = types.str;
-                  example = "api.smos.cs-syd.eu";
-                  description = "The url of the sync server";
-                };
-              username =
-                mkOption {
-                  type = types.str;
-                  example = "syd";
-                  description =
-                    "The username to use when logging into the sync server";
-                };
-              password =
-                mkOption {
-                  type = types.str;
-                  example = "hunter12";
-                  description =
-                    "The password to use when logging into the sync server";
-                };
-            };
-        }
-      );
     };
     calendar = mkOption {
-      default = null;
-      type = types.nullOr (
-        types.submodule {
-          options = {
-            enable = mkEnableOption "Smos calendar importing";
-            sources = mkOption {
-              description = "The list of sources to import from";
-              default = [ ];
-              type = types.listOf (
-                types.submodule {
-                  options = {
-                    name = mkOption {
-                      type = types.nullOr types.str;
-                      default = null;
-                      example = "Personal";
-                      description = "The name of the source";
-                    };
-                    destination = mkOption {
-                      type = types.str;
-                      default = null;
-                      example = "calendar/name.smos";
-                      description = "The destination file within the workflow directory";
-                    };
-                    source = mkOption {
-                      type = types.str;
-                      default = null;
-                      example = "https://calendar.google.com/calendar/ical/xxx.xxxxxxxxx%40gmail.com/private-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/basic.ics";
-                      description = "The url to download the calendar from";
-                    };
-                  };
-                }
-              );
-            };
+      description = "Periodic calendar imports";
+      type = types.nullOr (types.submodule {
+        options = {
+          enable = mkEnableOption "Smos calendar importing";
+          sources = mkOption {
+            description = "The list of sources to import from";
+            default = [ ];
+            type = types.listOf (types.submodule {
+              options = {
+                name = mkOption {
+                  type = types.nullOr types.str;
+                  default = null;
+                  example = "Personal";
+                  description = "The name of the source";
+                };
+                destination = mkOption {
+                  type = types.str;
+                  default = null;
+                  example = "calendar/name.smos";
+                  description = "The destination file within the workflow directory";
+                };
+                source = mkOption {
+                  type = types.str;
+                  default = null;
+                  example = "https://calendar.google.com/calendar/ical/xxx.xxxxxxxxx%40gmail.com/private-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/basic.ics";
+                  description = "The url to download the calendar from";
+                };
+              };
+            });
           };
-        }
-      );
+        };
+      });
+      default = null;
     };
     scheduler = mkOption {
-      default = null;
-      type = types.nullOr (
-        types.submodule {
-          options = {
-            enable = mkEnableOption "Smos scheduler activation";
-            schedule = mkOption {
-              description = "The schedule to activate";
-              default = [ ];
-              type = types.listOf (
-                types.submodule {
-                  options = {
-                    description = mkOption {
-                      type = types.nullOr types.str;
-                      default = null;
-                      example = "Weekly tasks for work";
-                      description = "A description of the schedule item. This is only used for logging and error messages.";
-                    };
-                    template = mkOption {
-                      type = types.nullOr types.str;
-                      default = null;
-                      example = "templates/weekly.smos";
-                      description = "The relative path to the template in the workflow dir";
-                    };
-                    destination = mkOption {
-                      type = types.str;
-                      default = null;
-                      example = "workflow/work-[ %Y-%V | monday ].smos";
-                      description = "The template relative path to the destination in the workflow dir";
-                    };
-                    schedule = mkOption {
-                      type = types.str;
-                      default = null;
-                      example = "0 12 * * 6"; # At 12:00 on saturday
-                      description = "The cron schedule for when to activate this item";
-                    };
-                  };
-                }
-              );
-            };
+      description = "Automatic scheduled project scheduling";
+      type = types.nullOr (types.submodule {
+        options = {
+          enable = mkEnableOption "Smos scheduler activation";
+          schedule = mkOption {
+            description = "The schedule to activate";
+            default = [ ];
+            type = types.listOf (types.submodule {
+              options = {
+                description = mkOption {
+                  type = types.nullOr types.str;
+                  default = null;
+                  example = "Weekly tasks for work";
+                  description = "A description of the schedule item. This is only used for logging and error messages.";
+                };
+                template = mkOption {
+                  type = types.nullOr types.str;
+                  default = null;
+                  example = "templates/weekly.smos";
+                  description = "The relative path to the template in the workflow dir";
+                };
+                destination = mkOption {
+                  type = types.str;
+                  default = null;
+                  example = "workflow/work-[ %Y-%V | monday ].smos";
+                  description = "The template relative path to the destination in the workflow dir";
+                };
+                schedule = mkOption {
+                  type = types.str;
+                  default = null;
+                  example = "0 12 * * 6"; # At 12:00 on saturday
+                  description = "The cron schedule for when to activate this item";
+                };
+              };
+            });
           };
-        }
-      );
+        };
+      });
+      default = null;
     };
     notify = mkOption {
+      description = "Desktop notifications";
+      type = types.nullOr (types.submodule {
+        options = {
+          enable = mkEnableOption "Smos notification activation";
+        };
+      });
       default = null;
-      type =
-        types.nullOr (types.submodule {
-          options = {
-            enable = mkEnableOption "Smos notification activation";
-          };
-        });
     };
   };
   config =
