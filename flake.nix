ad26ac52769068607b8424f42eaeb938b47b6192
Daniel Firth <dan.firth@homotopic.tech>
2022-10-07 13:53:50 +0100

    Add more flake packages

--- flake.nix
+++ flake.nix
@@ -25,23 +25,31 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      hsPkgs = pkgs.haskell.packages.ghc942.override {
-        overrides = hfinal: hprev:
-          horizon-platform.packages.${system} //
-          {
-            smos-report = hprev.callCabal2nix "smos-report" ./smos-report { };
-          };
+      hsPkgs =
+        with pkgs.haskell.lib.compose;
+        pkgs.haskell.packages.ghc942.override {
+          overrides = hfinal: hprev:
+            horizon-platform.packages.${system} //
+            {
+              smos-archive = addBuildTool hprev.autoexporter (hprev.callCabal2nix "smos-archive" ./smos-archive { });
+              smos-cursor = addBuildTool hprev.autoexporter (hprev.callCabal2nix "smos-cursor" ./smos-cursor { });
+              smos-cursor-gen = addBuildTool hprev.autoexporter (hprev.callCabal2nix "smos-cursor-gen" ./smos-cursor-gen { });
+              smos-report = hprev.callCabal2nix "smos-report" ./smos-report { };
+              smos-report-cursor = hprev.callCabal2nix "smos-report-cursor" ./smos-report-cursor { };
+              smos-report-gen = hprev.callCabal2nix "smos-report-gen" ./smos-report-gen { };
+              smos = hprev.callCabal2nix "smos" ./smos { };
+            };
       };
     in
     {
-      devShells.default = hsPkgs.smos-report.env.overrideAttrs (attrs: {
+      devShells.default = hsPkgs.smos-archive.env.overrideAttrs (attrs: {
         buildInputs = attrs.buildInputs ++ [
           hsPkgs.cabal-install
           hsPkgs.stylish-haskell
           pkgs.nixpkgs-fmt
         ];
       });
-      packages.default = hsPkgs.smos-report;
+      packages.default = hsPkgs.smos;
       checks =
         {
           hlint = lint-utils.outputs.linters.${system}.hlint self;
