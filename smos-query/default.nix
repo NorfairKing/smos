5ce705c59cb398309904f90d2349ac3baaa71654
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-03-21 16:38:12 +0100

    Removed now-unnecessary tests and instances

--- smos-query/default.nix
+++ smos-query/default.nix
@@ -1,12 +1,10 @@
 { mkDerivation, aeson, aeson-pretty, autodocodec, autoexporter
-, base, bytestring, conduit, containers, envparse, genvalidity
-, genvalidity-containers, genvalidity-sydtest
-, genvalidity-sydtest-aeson, lib, mtl, optparse-applicative, path
+, base, bytestring, conduit, containers, envparse
+, genvalidity-sydtest, lib, mtl, optparse-applicative, path
 , path-io, pretty-relative-time, safe-coloured-text
-, safe-coloured-text-layout, safe-coloured-text-layout-gen
-, safe-coloured-text-terminfo, smos-cli, smos-data, smos-report
-, smos-report-gen, sydtest, sydtest-discover, text, time, tz
-, validity, validity-path, yaml
+, safe-coloured-text-layout, safe-coloured-text-terminfo, smos-cli
+, smos-data, smos-report, smos-report-gen, sydtest
+, sydtest-discover, text, time, tz, validity, validity-path, yaml
 }:
 mkDerivation {
   pname = "smos-query";
@@ -24,10 +22,8 @@ mkDerivation {
   libraryToolDepends = [ autoexporter ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
-    base containers genvalidity genvalidity-containers
-    genvalidity-sydtest genvalidity-sydtest-aeson path path-io
-    safe-coloured-text-layout-gen smos-cli smos-report smos-report-gen
-    sydtest text yaml
+    base containers genvalidity-sydtest path path-io smos-report
+    smos-report-gen sydtest text yaml
   ];
   testToolDepends = [ sydtest-discover ];
   license = lib.licenses.mit;
