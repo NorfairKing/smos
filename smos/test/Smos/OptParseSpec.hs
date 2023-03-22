819b30efc20160409a67a872bc000c2648b302ad
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-03-22 11:27:26 +0100

    Removed some now-obsolete tests

--- smos/test/Smos/OptParseSpec.hs
+++ smos/test/Smos/OptParseSpec.hs
@@ -11,29 +11,17 @@ import TestImport
 spec :: Spec
 spec = do
   genValidSpec @KeyConfig
-  jsonSpec @KeyConfig
   genValidSpec @KeyConfigs
-  jsonSpec @KeyConfigs
   genValidSpec @FileKeyConfigs
-  jsonSpec @FileKeyConfigs
   genValidSpec @BrowserKeyConfigs
-  jsonSpec @BrowserKeyConfigs
   genValidSpec @HelpKeyConfigs
-  jsonSpec @HelpKeyConfigs
   genValidSpec @ReportsKeyConfigs
-  jsonSpec @ReportsKeyConfigs
   genValidSpec @NextActionReportKeyConfigs
-  jsonSpec @NextActionReportKeyConfigs
   genValidSpec @WaitingReportKeyConfigs
-  jsonSpec @WaitingReportKeyConfigs
   genValidSpec @TimestampsReportKeyConfigs
-  jsonSpec @TimestampsReportKeyConfigs
   genValidSpec @KeybindingsConfiguration
-  jsonSpec @KeybindingsConfiguration
   genValidSpec @Configuration
-  jsonSpec @Configuration
   describe "FromJSON Configuration" $
-    it "parses the default configuration back into itself" $
-      do
-        let config = backToConfiguration defaultConfig
-        fromJSON (toJSON config) `shouldBe` JSON.Success config
+    it "parses the default configuration back into itself" $ do
+      let config = backToConfiguration defaultConfig
+      fromJSON (toJSON config) `shouldBe` JSON.Success config
