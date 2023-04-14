c91fba86961cf8ab6c89c470a9b3db93b6f3a850
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-04-14 14:38:33 +0200

    remove now-obsolete json test in smos-api-gen

--- smos-api-gen/default.nix
+++ smos-api-gen/default.nix
@@ -1,11 +1,10 @@
 { mkDerivation, aeson, base, bytestring, containers, criterion
 , dirforest, genvalidity, genvalidity-bytestring
 , genvalidity-criterion, genvalidity-mergeful, genvalidity-path
-, genvalidity-sydtest, genvalidity-sydtest-aeson, genvalidity-text
-, genvalidity-typed-uuid, genvalidity-uuid, lib, mergeful, path
-, QuickCheck, semver, smos-api, smos-data, smos-data-gen
-, smos-report, sydtest, sydtest-aeson, sydtest-discover, text, time
-, typed-uuid, uuid
+, genvalidity-sydtest, genvalidity-text, genvalidity-typed-uuid
+, genvalidity-uuid, lib, mergeful, path, QuickCheck, semver
+, smos-api, smos-data, smos-data-gen, smos-report, sydtest
+, sydtest-aeson, sydtest-discover, text, time, typed-uuid, uuid
 }:
 mkDerivation {
   pname = "smos-api-gen";
@@ -18,8 +17,8 @@ mkDerivation {
   ];
   testHaskellDepends = [
     aeson base bytestring containers dirforest genvalidity-sydtest
-    genvalidity-sydtest-aeson mergeful path semver smos-api smos-data
-    smos-report sydtest sydtest-aeson time typed-uuid uuid
+    mergeful path semver smos-api smos-data smos-report sydtest
+    sydtest-aeson time typed-uuid uuid
   ];
   testToolDepends = [ sydtest-discover ];
   benchmarkHaskellDepends = [
--- smos-api-gen/test/Smos/APISpec.hs
+++ smos-api-gen/test/Smos/APISpec.hs
@@ -1,29 +1,18 @@
 {-# LANGUAGE TypeApplications #-}
 
-module Smos.APISpec
-  ( spec,
-  )
-where
+module Smos.APISpec (spec) where
 
 import Smos.API
 import Smos.API.Gen ()
 import Test.Syd
 import Test.Syd.Validity
-import Test.Syd.Validity.Aeson
 
 spec :: Spec
 spec = do
   genValidSpec @Username
-  jsonSpec @Username
   genValidSpec @Register
-  jsonSpec @Register
   genValidSpec @SyncFile
-  jsonSpec @SyncFile
   genValidSpec @SyncRequest
-  jsonSpec @SyncRequest
   genValidSpec @SyncResponse
-  jsonSpec @SyncResponse
   genValidSpec @BackupInfo
-  jsonSpec @BackupInfo
   genValidSpec @UserInfo
-  jsonSpec @UserInfo
