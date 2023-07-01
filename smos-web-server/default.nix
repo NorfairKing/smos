69e006be685d3c84f389fa846cd67496323447f6
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-07-01 21:00:21 +0200

    fix the bug

--- smos-web-server/default.nix
+++ smos-web-server/default.nix
@@ -2,14 +2,14 @@
 , base, base16-bytestring, bytestring, conduit, containers
 , data-default, deepseq, envparse, genvalidity-sydtest, http-client
 , http-client-tls, http-types, ical, lib, monad-logger, mtl
-, network-uri, optparse-applicative, path, path-io
-, persistent-sqlite, pretty-relative-time, pretty-show, QuickCheck
-, servant, servant-auth-client, servant-client, shakespeare, smos
-, smos-cli, smos-client, smos-data, smos-data-gen, smos-directory
+, optparse-applicative, path, path-io, persistent-sqlite
+, pretty-relative-time, pretty-show, QuickCheck, servant
+, servant-auth-client, servant-client, shakespeare, smos, smos-cli
+, smos-client, smos-data, smos-data-gen, smos-directory
 , smos-report, smos-server-gen, smos-sync-client, smos-web-style
 , stm, sydtest, sydtest-discover, sydtest-yesod, template-haskell
-, text, time, typed-uuid, tz, unliftio, wai-extra, warp, websockets
-, yaml, yesod, yesod-auth, yesod-autoreload, yesod-static
+, text, time, typed-uuid, tz, unliftio, wai-extra, warp, yaml
+, yesod, yesod-auth, yesod-autoreload, yesod-static
 , yesod-static-remote, yesod-websockets
 }:
 mkDerivation {
@@ -34,9 +34,9 @@ mkDerivation {
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
     base containers genvalidity-sydtest http-client http-types
-    monad-logger mtl network-uri path path-io QuickCheck servant-client
-    smos-client smos-data smos-data-gen smos-server-gen smos-web-style
-    stm sydtest sydtest-yesod text time websockets yesod-auth
+    monad-logger QuickCheck servant-client smos-client smos-data-gen
+    smos-server-gen smos-web-style stm sydtest sydtest-yesod text
+    yesod-auth
   ];
   testToolDepends = [ sydtest-discover ];
   license = lib.licenses.mit;
