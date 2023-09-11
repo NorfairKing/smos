a19965521714ef2d83327ba0d8b7cc0917ecdba5
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-09-11 12:27:57 +0200

    test the ongoing report parsing

---
+++ smos-report-gen/test/Smos/Report/OngoingSpec.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Smos.Report.OngoingSpec where
+
+import qualified Data.Map as M
+import Data.Time
+import Data.Time.Zones.All
+import Smos.Data
+import Smos.Directory.Archive.Gen ()
+import Smos.Directory.ShouldPrint
+import Smos.Directory.TestUtils
+import Smos.Report.Filter.Gen ()
+import Smos.Report.Ongoing
+import Smos.Report.Ongoing.Gen ()
+import Test.Syd
+import Test.Syd.Validity
+import Test.Syd.Validity.Aeson
+
+spec :: Spec
+spec = do
+  genValidSpec @OngoingReport
+  jsonSpec @OngoingReport
+  genValidSpec @OngoingEntry
+  jsonSpec @OngoingEntry
+  modifyMaxSuccess (`div` 10) $
+    describe "produceOngoingReport" $
+      it "produces valid reports for interesting stores" $
+        forAllValid $ \zone ->
+          forAllValid $ \now ->
+            forAllValid $ \mFilter ->
+              forAllValid $ \ha ->
+                withInterestingStore $ \dc -> do
+                  wr <- produceOngoingReport zone now mFilter ha DontPrint dc
+                  shouldBeValid wr
+
+  describe "parseOngoingEntry" $ do
+    it "always uses the appropriate filepath and header if it parses something" $
+      forAllValid $ \zone ->
+        forAllValid $ \now ->
+          forAllValid $ \rf ->
+            forAllValid $ \e -> do
+              case parseOngoingEntry zone now rf e of
+                Nothing -> pure () -- Fine
+                Just OngoingEntry {..} -> do
+                  ongoingEntryFilePath `shouldBe` rf
+                  ongoingEntryHeader `shouldBe` entryHeader e
+
+    let zone = tzByLabel Europe__Zurich
+        now = UTCTime (fromGregorian 2023 09 11) (timeOfDayToTime (TimeOfDay 12 14 00))
+
+    it "can parse a OnlyBegin" $
+      forAllValid $ \rf ->
+        forAllValid $ \h -> do
+          let begin = TimestampDay (fromGregorian 2023 09 10)
+              e = (newEntry h) {entryTimestamps = M.fromList [("BEGIN", begin)]}
+          case parseOngoingEntry zone now rf e of
+            Nothing -> expectationFailure "Should have found an OngoingEntry"
+            Just OngoingEntry {..} -> ongoingEntryBeginEnd `shouldBe` OnlyBegin begin
+
+    it "can parse a OnlyEnd" $
+      forAllValid $ \rf ->
+        forAllValid $ \h -> do
+          let end = TimestampDay (fromGregorian 2023 09 15)
+              e = (newEntry h) {entryTimestamps = M.fromList [("END", end)]}
+          case parseOngoingEntry zone now rf e of
+            Nothing -> expectationFailure "Should have found an OngoingEntry"
+            Just OngoingEntry {..} -> ongoingEntryBeginEnd `shouldBe` OnlyEnd end
+
+    it "can parse a BeginEnd" $
+      forAllValid $ \rf ->
+        forAllValid $ \h -> do
+          let begin = TimestampDay (fromGregorian 2023 09 09)
+              end = TimestampDay (fromGregorian 2023 09 16)
+              e = (newEntry h) {entryTimestamps = M.fromList [("BEGIN", begin), ("END", end)]}
+          case parseOngoingEntry zone now rf e of
+            Nothing -> expectationFailure "Should have found an OngoingEntry"
+            Just OngoingEntry {..} -> ongoingEntryBeginEnd `shouldBe` BeginEnd begin end
