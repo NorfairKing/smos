c50daa10bdbbe395865178a12ee78700f44ee91a
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-01-16 00:54:31 +0100

    complete the sitemap

--- smos-docs-site/src/Smos/Docs/Site/Handler/Sitemap.hs
+++ smos-docs-site/src/Smos/Docs/Site/Handler/Sitemap.hs
@@ -22,90 +22,95 @@ getSitemapR = do
 
 getUrls :: Load [SitemapUrl (Route App)]
 getUrls =
-  ( \dps ->
+  ( \dps cls ->
       concat
         [ [ SitemapUrl
               { sitemapLoc = HomeR,
                 sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Weekly,
+                sitemapChangeFreq = Just Yearly,
                 sitemapPriority = Just 0.9
+              },
+            SitemapUrl
+              { sitemapLoc = ChangelogR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = ChangelogLatestR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = ChangelogAllR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              }
+          ],
+          map
+            ( \day ->
+                SitemapUrl
+                  { sitemapLoc = ChangelogReleaseR day,
+                    sitemapLastMod = Nothing,
+                    sitemapChangeFreq = Just Never,
+                    sitemapPriority = Just 0.1
+                  }
+            )
+            cls,
+          [ SitemapUrl
+              { sitemapLoc = CurrentVersionsR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.4
               },
             SitemapUrl
               { sitemapLoc = SmosR,
                 sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
+                sitemapChangeFreq = Just Yearly,
                 sitemapPriority = Just 0.5
               },
             SitemapUrl
               { sitemapLoc = SmosFileR,
                 sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
+                sitemapChangeFreq = Just Yearly,
                 sitemapPriority = Just 0.5
               },
             SitemapUrl
               { sitemapLoc = SmosKeybindingsR,
                 sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
+                sitemapChangeFreq = Just Yearly,
                 sitemapPriority = Just 0.5
               },
             SitemapUrl
               { sitemapLoc = SmosActionsR,
                 sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
-                sitemapPriority = Just 0.5
-              },
-            SitemapUrl
-              { sitemapLoc = SmosSingleR,
-                sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
-                sitemapPriority = Just 0.5
-              },
-            SitemapUrl
-              { sitemapLoc = SmosArchiveR,
-                sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
-                sitemapPriority = Just 0.5
-              },
-            SitemapUrl
-              { sitemapLoc = SmosCalendarImportR,
-                sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
-                sitemapPriority = Just 0.5
-              },
-            SitemapUrl
-              { sitemapLoc = SmosSchedulerR,
-                sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
-                sitemapPriority = Just 0.5
-              },
-            SitemapUrl
-              { sitemapLoc = SmosSchedulerTemplateR,
-                sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
-                sitemapPriority = Just 0.5
-              },
-            SitemapUrl
-              { sitemapLoc = SmosSyncR,
-                sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
-                sitemapPriority = Just 0.5
-              },
-            SitemapUrl
-              { sitemapLoc = SmosServerR,
-                sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
-                sitemapPriority = Just 0.5
-              },
-            SitemapUrl
-              { sitemapLoc = SmosWebServerR,
-                sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
+                sitemapChangeFreq = Just Yearly,
                 sitemapPriority = Just 0.5
               },
             SitemapUrl
               { sitemapLoc = SmosQueryR,
                 sitemapLastMod = Nothing,
-                sitemapChangeFreq = Just Monthly,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosQueryFilterR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosQueryColumnR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosQuerySorterR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
                 sitemapPriority = Just 0.5
               }
           ],
@@ -114,23 +119,190 @@ getUrls =
                 SitemapUrl
                   { sitemapLoc = SmosQueryCommandR command,
                     sitemapLastMod = Nothing,
-                    sitemapChangeFreq = Just Monthly,
+                    sitemapChangeFreq = Just Yearly,
                     sitemapPriority = Just 0.4
                   }
             )
-            [ "next",
-              "work",
+            [ "entry",
+              "report",
+              "waiting",
+              "next",
+              "ongoing",
+              "clock",
               "agenda",
               "projects",
               "stuck",
-              "waiting",
-              "clock",
+              "work",
+              "free",
               "log",
               "stats",
-              "tags",
-              "report",
-              "entry"
+              "tags"
             ],
+          [ SitemapUrl
+              { sitemapLoc = SmosSingleR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosArchiveR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              }
+          ],
+          map
+            ( \command ->
+                SitemapUrl
+                  { sitemapLoc = SmosArchiveCommandR command,
+                    sitemapLastMod = Nothing,
+                    sitemapChangeFreq = Just Yearly,
+                    sitemapPriority = Just 0.4
+                  }
+            )
+            [ "file",
+              "export"
+            ],
+          [ SitemapUrl
+              { sitemapLoc = SmosNotifyR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosCalendarImportR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosSchedulerR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosSchedulerTemplateR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosSchedulerNixosR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              }
+          ],
+          map
+            ( \command ->
+                SitemapUrl
+                  { sitemapLoc = SmosSchedulerCommandR command,
+                    sitemapLastMod = Nothing,
+                    sitemapChangeFreq = Just Yearly,
+                    sitemapPriority = Just 0.4
+                  }
+            )
+            [ "check",
+              "sample",
+              "schedule",
+              "next"
+            ],
+          [ SitemapUrl
+              { sitemapLoc = SmosSyncR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosSyncNixosR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              }
+          ],
+          map
+            ( \command ->
+                SitemapUrl
+                  { sitemapLoc = SmosSyncCommandR command,
+                    sitemapLastMod = Nothing,
+                    sitemapChangeFreq = Just Yearly,
+                    sitemapPriority = Just 0.4
+                  }
+            )
+            [ "register",
+              "login",
+              "sync"
+            ],
+          [ SitemapUrl
+              { sitemapLoc = SmosServerR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosWebServerR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = SmosGitHubR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              }
+          ],
+          map
+            ( \command ->
+                SitemapUrl
+                  { sitemapLoc = SmosGitHubCommandR command,
+                    sitemapLastMod = Nothing,
+                    sitemapChangeFreq = Just Yearly,
+                    sitemapPriority = Just 0.4
+                  }
+            )
+            [ "list",
+              "import"
+            ],
+          [ SitemapUrl
+              { sitemapLoc = SmosJobHuntR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              }
+          ],
+          map
+            ( \command ->
+                SitemapUrl
+                  { sitemapLoc = SmosJobHuntCommandR command,
+                    sitemapLastMod = Nothing,
+                    sitemapChangeFreq = Just Yearly,
+                    sitemapPriority = Just 0.4
+                  }
+            )
+            [ "init",
+              "email"
+            ],
+          [ SitemapUrl
+              { sitemapLoc = NixosModuleR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = HomeManagerModuleR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              },
+            SitemapUrl
+              { sitemapLoc = DependencyGraphR,
+                sitemapLastMod = Nothing,
+                sitemapChangeFreq = Just Yearly,
+                sitemapPriority = Just 0.5
+              }
+          ],
           map
             ( \(urlPieces, _) ->
                 SitemapUrl
@@ -144,3 +316,4 @@ getUrls =
         ]
   )
     <$> docPages
+    <*> (M.keys <$> changelogs)
