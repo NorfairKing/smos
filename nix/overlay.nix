453d18a888ec7b4830592c71c245e3eab0cf3fd5
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-03-06 00:19:38 +0100

    trying a bulma fix

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -170,13 +170,13 @@ in
         url = "https://github.com/jgthms/bulma";
         rev = "c02757cd3043a4b30231c72dd01cd735c3b3672c";
       };
-      bulma-carousel = builtins.fetchGit {
-        url = "https://github.com/Wikiki/bulma-carousel";
-        rev = "71e38451f429af74aa8dd6c0d69ce9dd626f87f6";
+      bulma-carousel = final.fetchurl {
+        url = "https://registry.npmjs.org/bulma-carousel/-/bulma-carousel-4.0.24.tgz";
+        sha256 = "sha256-PoeLjz5RKJnlPhL1aTjrxgfpBDVGn39bnPjR/KfsZe0=";
       };
-      bulma-pricingtable = builtins.fetchGit {
-        url = "https://github.com/Wikiki/bulma-pricingtable";
-        rev = "25ef9a4e97afd2da9bd92d3e1c83fbd0caf91102";
+      bulma-pricingtable = final.fetchurl {
+        url = "https://registry.npmjs.org/bulma-pricingtable/-/bulma-pricingtable-0.2.0.tgz";
+        sha256 = "sha256-KycN76zCdymlRSPk75hxyQ4FGi4ldICVUr6ZYGeWCZw";
       };
     in
     final.stdenv.mkDerivation {
